9c73201282ae6fbfb8cf7569a9f0af32166d5bfb
rjsteinert <rj@rjsteinert.com>
2013-05-06 16:35:20 -0400

    File attachments for Resources

--- attachments/app/views/CouchHubResourceFormView.js
+++ attachments/app/views/CouchHubResourceFormView.js
@@ -16,10 +16,12 @@ $(function() {
 
         // Extra elements not covered in the schema
         var $button = $('<button type="button" name="save" id="formButton">save</button>')
+        var $file = $('<form method="post" id="fileAttachment"><input type="file" name="_attachments" id="_attachments" multiple="multiple" /> <input class="rev" type="hidden" name="_rev"></form>')
 
         this.form = new Backbone.Form({ model: this.model })
 
         $(this.el).append(this.form.render().el)
+        $(this.el).append($file)
         $(this.el).append($button)
 
         return this;
@@ -28,7 +30,6 @@ $(function() {
 
       },
 
-
       saveForm: function() {
 
           // Put the form's input into the model in memory
@@ -36,10 +37,21 @@ $(function() {
           // Send the updated model to the server
           var that = this
           this.model.save(null, {success: function() {
-            window.location = 'couch-hub-resources.html'
+            that.saveFileAttachment()
           }})
 
       },
+
+      saveFileAttachment: function() {
+        $("form#fileAttachment .rev").attr('value', this.model.get('_rev'))
+        var that = this
+        $('form#fileAttachment').ajaxSubmit({
+          url: "/"+ that.model._db.name +"/"+ that.model.get('_id'),
+          success: function(response) {
+            window.location = 'couch-hub-resources.html'
+          }
+        })        
+      }
 
 
     });
