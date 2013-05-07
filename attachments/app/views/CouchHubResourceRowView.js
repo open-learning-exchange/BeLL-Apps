c308ecd1c20d5ffb28499cba390ae498a75aac84
rjsteinert <rj@rjsteinert.com>
2013-05-07 13:43:07 -0400

    Moved CouchHubResourceRowView template to DOM

--- attachments/app/views/CouchHubResourceRowView.js
+++ attachments/app/views/CouchHubResourceRowView.js
@@ -8,13 +8,14 @@ var CouchHubResourceRowView = Backbone.View.extend({
     }
   },
 
+  template : _.template($("#template-CouchHubResourceRowView").html()),
+
   initialize: function() {
     this.model.on('destroy', this.remove, this)
   },
-
+  
   render: function () {
-    var template = "<td><%= name %></td> <td><a class='btn' href='" + this.openWith() + "'><i class='icon-eye-open'></i>Open</a></td> <td><a class='btn' href='edit-couch-hub-resource.html?id=<%= id %>'><i class='icon-pencil'></i>edit</a></td><td><a class='destroy btn' href='#'><i class='icon-remove'></i>delete</a></td>"
-    this.$el.append(_.template(template, this.model.toJSON()))
+    this.$el.append(this.template(this.model.toJSON()))
   },
 
   openWith: function() {
