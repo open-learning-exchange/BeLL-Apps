e2f2f11c91ce028542c131016736a5c8d4ea5c7a
rjsteinert <rj@rjsteinert.com>
2013-05-07 11:34:57 -0400

    Resource browser opens in modal now

--- attachments/app/views/CouchHubRowView.js
+++ attachments/app/views/CouchHubRowView.js
@@ -5,6 +5,9 @@ var CouchHubRowView = Backbone.View.extend({
   events: {
     "click .destroy" : function() {
       this.model.destroy()
+    },
+    "click .trigger-modal" : function() {
+      $('#myModal').modal({show:true})
     }
   },
 
@@ -13,7 +16,7 @@ var CouchHubRowView = Backbone.View.extend({
   },
 
   render: function () {
-    var template = "<td><%= name %></td> <td><a href='/<%= database %>/_design/couch-hub/pages/couch-hub-resources.html' target='frame'><i class='icon-list'></i> Browse Resources</a></td> <td><a href='edit-couch-hub.html?id=<%= id %>'><i class='icon-pencil'></i> Edit</a></td> <td><a class='destroy' href='#'><i class='icon-remove'></i> Delete</a></td>"
+    var template = "<td><%= name %></td> <td><a role='button' class='btn trigger-modal' href='/<%= database %>/_design/couch-hub/pages/couch-hub-resources.html' target='frame'><i class='icon-list'></i> Browse Resources</a></a></td> <td><a href='edit-couch-hub.html?id=<%= id %>'><i class='icon-pencil'></i> Edit</a></td> <td><a class='destroy' href='#'><i class='icon-remove'></i> Delete</a></td>"
     this.$el.append(_.template(template, this.model.toJSON()))
   }
 
