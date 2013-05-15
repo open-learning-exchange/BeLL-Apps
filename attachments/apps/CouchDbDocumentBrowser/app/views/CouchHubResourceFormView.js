7e45c85eb0ac67af37f800114b4858ef1b147566
rjsteinert <rj@rjsteinert.com>
2013-05-15 12:14:38 -0400

    Document browser apps added

---
+++ attachments/apps/CouchDbDocumentBrowser/app/views/CouchHubResourceFormView.js
@@ -0,0 +1,47 @@
+$(function() {
+
+    window.CouchHubResourceFormView = Backbone.View.extend({
+
+      events: {
+        "click button#formButton": "saveForm"
+      },
+
+      render: function() {
+
+        this.buildForm()
+
+      },
+
+      buildForm: function() {
+
+        // Extra elements not covered in the schema
+        var $button = $('<button type="button" name="save" id="formButton">save</button>')
+
+        this.form = new Backbone.Form({ model: this.model })
+
+        $(this.el).append(this.form.render().el)
+        $(this.el).append($button)
+
+        return this;
+
+
+
+      },
+
+
+      saveForm: function() {
+
+          // Put the form's input into the model in memory
+          this.form.commit()
+          // Send the updated model to the server
+          var that = this
+          this.model.save(null, {success: function() {
+            window.location = 'couch-hubs.html'
+          }})
+
+      },
+
+
+    });
+
+});
