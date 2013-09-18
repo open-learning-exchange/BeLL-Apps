5ef8ed28c4c03e7f4749ed7c30bf74df87e86f05
rjsteinert <rj@rjsteinert.com>
2013-09-18 17:18:39 -0400

    Member model.

---
+++ attachments/lms/app/models/Member.js
@@ -0,0 +1,44 @@
+$(function() {
+
+  App.Models.Members = Backbone.Model.extend({
+
+    idAttribute: "_id",
+
+    url: function() {
+      if (_.has(this, 'id')) {
+        var url = (_.has(this.toJSON(), '_rev'))
+          ? App.Server + '/members/' + this.id + '?rev=' + this.get('_rev') // For UPDATE and DELETE
+          : App.Server + '/members/' + this.id // For READ
+      }
+      else {
+        var url = App.Server + '/members' // for CREATE
+      }
+      return url
+    },
+
+    defaults: {
+      kind: "Member"
+    },
+
+    schema: {
+      login: 'Text',
+      pass: 'Text',
+      firstName: 'Text',
+      lastName: 'Text',
+      middleNames: 'Text',
+      phone: 'Text',
+      email: 'Text',
+      levels: {
+        type: 'Checkboxes',
+        options: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+      },
+      roles: {
+        type: 'Checkboxes',
+        options: ['student', 'teacher', 'head', 'coach', 'lead']
+      }
+
+    },
+
+  }) 
+
+})
