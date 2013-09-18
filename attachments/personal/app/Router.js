604ce310a8b253d8996c62621c979d7ee4308bcd
rjsteinert <rj@rjsteinert.com>
2013-09-18 14:44:26 -0400

    Major uncrufting of the Personal BeLL app.

--- attachments/personal/app/Router.js
+++ attachments/personal/app/Router.js
@@ -2,47 +2,13 @@ $(function() {
   App.Router = new (Backbone.Router.extend({
 
     routes: {
-      ''                            : 'Groups', 
-      'resources'                   : 'Resources',
-      'resource/add'                : 'ResourceForm',
-      'resource/edit/:resourceId'   : 'ResourceForm',
+      ''                           : 'Groups', 
       'teams'                      : 'Groups',
       'team/edit/:groupId'         : 'GroupForm',
       'team/assign/:groupId'       : 'GroupAssignments', // @todo delete and change refs to it
       'team/assignments/:groupId'  : 'GroupAssignments',
-      'team/add'                   : 'GroupAdd',
-      'team/link/:groupId'                  : 'GroupLink',
-      'update-assignments'                  : 'UpdateAssignments'
-    },
-
-    ResourceForm : function(resourceId) {
-      var resource = (resourceId)
-        ? new App.Models.Resource({_id: resourceId})
-        : new App.Models.Resource()
-      resource.on('processed', function() {
-        Backbone.history.navigate('resources', {trigger: true})
-      })
-      var resourceFormView = new App.Views.ResourceForm({model: resource})
-      App.$el.children('.body').html(resourceFormView.el)
-      if(resource.id) {
-        App.listenToOnce(resource, 'sync', function() {
-          resourceFormView.render()
-        })
-        resource.fetch()
-      }
-      else {
-        resourceFormView.render()
-      }
-    },
-
-    Resources: function(database) {
-      var resources = new App.Collections.Resources()
-      resources.fetch({success: function() {
-        var resourcesTableView = new App.Views.ResourcesTable({collection: resources})
-        resourcesTableView.render()
-        App.$el.children('.body').html('<h1>Resources</h1>')
-        App.$el.children('.body').append(resourcesTableView.el)
-      }})
+      'team/link/:groupId'         : 'GroupLink',
+      'update-assignments'         : 'UpdateAssignments'
     },
 
     Groups: function() {
@@ -53,24 +19,6 @@ $(function() {
         App.$el.children('.body').html('<h1>My Teams</h1>')
         App.$el.children('.body').append(groupsTable.el)
       }})
-    },
-
-    GroupAdd : function() {
-      // Set up the model
-      var group = new App.Models.Group()
-      // when the users submits the form, the group will be processed
-      group.on('processed', function() {
-        this.save()
-      })
-      // after this group is saved move on to the groups page
-      group.on('sync', function() {
-        Backbone.history.navigate('teams', {trigger: true})
-      })
-      // Set up the form
-      var groupForm = new App.Views.GroupForm({model: group})
-      groupForm.render()
-      App.$el.children('.body').html('<h1>Add a Team</h1>')
-      App.$el.children('.body').append(groupForm.el)
     },
 
     GroupAssignments: function(groupId) {
@@ -88,6 +36,15 @@ $(function() {
       })
       group.fetch()
     },
+
+    /*
+     * Syncing pages
+     * 
+     * @todo At the moment the syncing process replicates all databases from the server but 
+     * it should be selective according to the "linked group's assignments".  Linking more
+     * than one group is having some issues and the UpdateAssignments route will need to 
+     * be made much more intelligent.  
+     */
 
     GroupLink: function(groupId) {
       App.once('done:pull_doc_ids', function(){
