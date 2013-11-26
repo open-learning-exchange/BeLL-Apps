3ae331a2d7c2f176aec2db8cdaf15b353ef4cfc3
muhammad.khalid <muhammadkhalid@northbaysolutions.net>
2013-11-26 17:27:33 +0500

    Demo scree added in lms

---
+++ attachments/lms/app/views/Demo.js
@@ -0,0 +1,33 @@
+$(function() {
+
+  App.Views.Demo = Backbone.View.extend({
+
+    template: $('#template-demo').html(),
+
+    vars: {},
+
+    render: function() {
+      var dashboard = this
+        
+      // Member Name
+      var member = new App.Models.Member()
+      var that = this
+      this.vars = member.toJSON()
+      member.id = $.cookie('Member._id')
+      member.fetch({success: function() {
+      	that.vars.visits = member.get("visits")+1
+      	that.$el.html(_.template(that.template, that.vars))
+      	var vis = member.get('visits')
+      	member.set({"visits": vis + 1 })
+      	member.save({success:function(){
+                        
+        }})
+       // $('.name').html(member.get('firstName') + ' ' + member.get('lastName')+'<a href="#member/edit/'+$.cookie('Member._id')+'">[Edit]</a>')
+      }})
+      
+    }
+
+  })
+
+})
+
