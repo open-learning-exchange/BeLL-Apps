5b2883e132c84dad0c92f57b5d7bbd3d7c9bb9ba
Hassan Khan <hassan.khan@northbaysolutions.net>
2013-12-04 09:52:22 +0500

    - commit 3/12/2013 end

---
+++ attachments/personal/app/views/siteFeedbackPageRow.js
@@ -0,0 +1,40 @@
+$(function() {
+
+  App.Views.siteFeedbackPageRow = Backbone.View.extend({
+  template0 : $("#template-siteReviewRowAdmin").html(),
+  template1 : $("#template-siteReviewRownoAdmin").html(),
+   tagName: "tr",
+    authorName : null,
+   events: {
+      "click #resolveButton": "resolve",
+    },
+ 	
+ 	resolve: function(e){
+ 		console.log(e)
+ 		e.preventDefault()
+    	this.model.on('sync', function() {
+        location.reload(); 
+        })
+          this.model.save( {Resolved : "1"}, {success :function(){}});
+ 	},
+ 	
+   initialize: function(){
+     if(  $.cookie('Member.login')=='admin'){
+    this.template=this.template0
+   }
+  else{
+  	this.template=this.template1
+  }
+   
+   },
+   
+    render: function() {
+  	      var vars = this.model.toJSON()
+      console.log(vars)
+     this.$el.html(_.template(this.template, vars))
+    }
+
+  })
+
+})
+
