50c8af41de5ae4c6c7657a4c957885aa0f2a7bcb
muhammad.khalid <muhammadkhalid@northbaysolutions.net>
2013-12-11 18:16:09 +0500

    -Switch in Feedback

--- attachments/lms/app/views/siteFeedbackPage.js
+++ attachments/lms/app/views/siteFeedbackPage.js
@@ -6,10 +6,30 @@ $(function() {
     className: " table-feedback notification-table table-striped",
     authorName : null, 
     searchText : null,
+    resolved : null,
     stack : null,
      resultArray : null,
  	 events: {
  	 "click #see-all" : function(e) {
+ 	 	while(skipStack.length > 0 )
+      	{	
+            skipStack.pop();        
+     	}
+   		searchText = ""
+   		this.resultArray  = []
+ 	 	skip = 0
+ 	 	skipStack.push(skip)
+ 	 	this.fetchRecords()
+ 	 },
+ 	 "click #switch" : function(e) {
+ 	 	if(this.resolved=="0")
+ 	 	{
+ 	 		this.resolved = "1"
+ 	 	}
+ 	 	else
+ 	 	{
+ 	 		this.resolved = "0"
+ 	 	}
  	 	while(skipStack.length > 0 )
       	{	
             skipStack.pop();        
@@ -57,12 +77,16 @@ $(function() {
  	
    initialize: function(){
    	this.resultArray  = []
-   	searchText = ""
+   	this.resolved = "1"
+   	this.searchText = ""
    	
    },
    
 	addAll: function(){
-		this.$el.append('<h4>Keyword:&nbsp;<input class="form-control" type="text" placeholder="Search in comment" value="" size="30" id="searchText" style="height:24px;margin-top:1%;"></input>&nbsp;<span><button class="btn btn-info" id="search_feedback">Search</button>&nbsp;<button class="btn btn-info" id="see-all">See All</button></span></h4><br/>')
+		this.$el.html('<h4>Keyword:&nbsp;<input class="form-control" type="text" placeholder="Search in comment" value="" size="30" id="searchText" style="height:24px;margin-top:1%;"></input>&nbsp;<span><button class="btn btn-info" id="search_feedback">Search</button>&nbsp;<button class="btn btn-info" id="see-all">See All</button></span></h4><br/>')
+		
+		this.$el.append('<button class="btn btn-info" id="switch">Switch</button><br/><br/>')
+		
 		this.$el.append('<th ><h4>Feedback</h4></th><th ><h4>Status</h4></th>')
 		this.collection.forEach(this.addOne,this)
 		this.$el.append('<br/><br/><input type="button" class="btn btn-hg btn-primary" id="previousButton" value="< Previous"> &nbsp;&nbsp;&nbsp;<button class="btn btn-hg btn-primary" id="next_button" >Next  ></button>')
@@ -133,7 +157,7 @@ $(function() {
 	   _.each(resourceArray, function(result) {
 		if(result.get("comment") != null ){
 		 	skip++
-			if(result.get("comment").toLowerCase().indexOf(searchText.toLowerCase()) >=0 )
+			if(result.get("comment").toLowerCase().indexOf(searchText.toLowerCase()) >=0 && that.resolved == result.get("Resolved"))
 			{	  
 				if(resultArray.length < limitofRecords)
 				{
