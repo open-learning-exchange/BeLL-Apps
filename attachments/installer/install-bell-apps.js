15a4b1ad3d715bc304b799707c9e356b88b3f800
rjsteinert <rj@rjsteinert.com>
2014-01-06 22:42:46 -0500

    Added html and javascript for a Bookmarklet installer.

---
+++ attachments/installer/install-bell-apps.js
@@ -0,0 +1,30 @@
+var source = 'http://bellappssource:installpass@bellappssource.cloudant.com'
+var i = 0
+alert("Install script loaded. Now replicating data from " + source)
+// @todo Need the rest of the databases here
+var databases = ['apps', 'facilities', 'members', 'feedback']
+// A recursive function to replicate the databases one at a time
+var replicate = function() {
+  var database = databases[i]
+  $.couch.replicate(source + '/' + database, database, {
+    success: function() {
+      if (databases.length <= i) {
+        i++
+        replicate()
+      }
+      else {
+        alert("Installation has completed")
+        window.location = window.location.host + '/apps/_design/bell/lms/index.html'
+      }
+    },
+    error: function(status) {
+      console.log(status);
+      alert('Something went wrong:' + status)
+  	},
+  	{
+	    create_target: true
+  	}
+  })
+}
+// Start the recursion
+replicate()
