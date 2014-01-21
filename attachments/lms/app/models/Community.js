423e3c3dce42082f2e794ad15a4a7c0331ee3947
zmehmood <zagham.mehmood@northbaysolutions.net>
2014-01-21 20:09:01 +0500

    -sync resources with  somalia cummunities

--- attachments/lms/app/models/Community.js
+++ attachments/lms/app/models/Community.js
@@ -22,68 +22,17 @@ $(function() {
     },
 
     schema: {
-       	Name: {	type:'Text',
-       			validators: ['required']
+    
+    community_code: {	type:'Text',
+       					validators: ['required']
+       				  },
+       	nationName: {	type:'Text',
+       					validators: ['required']
        		  },
-        Url: {	type:'Text',
-       			validators: ['required']
-       		  },
-       SponserName: {	type:'Text',
-       			validators: ['required']
-       		  },
-        SponerAddress: {	type:'Text',
-       			validators: ['required']
+        nationUrl: {	type:'Text',
+       					validators: ['required']
        		  },
        
-        ContactFirstname: {	type:'Text',
-       			validators: ['required']
-       		  },
-        ContactMiddlename: {	type:'Text',
-       			validators: ['required']
-       		  },
-        ContactLastname: {	type:'Text',
-       			validators: ['required']
-       		  },
-        ContactPhone: {	type:'Text',
-       			validators: ['required']
-       		  },
-        ContactEmail: {	type:'Text',
-       			validators: ['required']
-       		  },
-        LeaderFirstname: {	type:'Text',
-       			validators: ['required']
-       		  },
-       	LeaderMiddlename: {	type:'Text',
-       			validators: ['required']
-       		  },	  
-       	LeaderLastname: {	type:'Text',
-       			validators: ['required']
-       		  },	  
-       	LeaderPhone: {	type:'Text',
-       			validators: ['required']
-       		  },	  
-       	LeaderEmail: {	type:'Text',
-       			validators: ['required']
-       		  },	  
-       	LeaderId: {	type:'Text',
-       			validators: ['required']
-       		  },	  	  
-        LeaderPassword: {	type:'Text',
-       			validators: ['required']
-       		  },
-       	UrgentName: {	type:'Text',
-       			validators: ['required']
-       		  },
-        UrgentPhone: {	type:'Text',
-       			validators: ['required']
-       		  },
-       	AuthName: {	type:'Text',
-       			validators: ['required']
-       		  },
-        AuthDate: {	type:'Text',
-       			validators: ['required']
-       		  },	  	  
-       		  	  
     },
 
   }) 
