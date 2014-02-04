641506288a533d358233b1f0018886ee60b6cd78
zmehmood <zagham.mehmood@northbaysolutions.net>
2014-02-04 17:31:42 +0500

    -meetup additions -time picker and

--- attachments/personal/app/collections/UserMeetups.js
+++ attachments/personal/app/collections/UserMeetups.js
@@ -4,7 +4,9 @@ $(function () {
 
 
         url: function () {
-              if(this.memberId)
+              if(this.memberId && this.meetupId)
+                 return App.Server + '/usermeetups/_design/bell/_view/getUserMeetup?key=["' + this.memberId + '","'+this.meetupId+'"]&include_docs=true' 
+              else if(this.memberId)
                  return App.Server + '/usermeetups/_design/bell/_view/getUsermeetups?key="' + this.memberId + '"&include_docs=true' 
              else
                return App.Server + '/usermeetups/_design/bell/_view/getMeetupUsers?key="' + this.meetupId + '"&include_docs=true'
