a5685c740ba4f9029b74da9bd4df73a7134bef73
tabraiz ahmad <tabraiz.ahmad@IMAC006.local>
2014-03-14 12:13:18 +0500

    Merging collections

---
+++ attachments/MyApp/app/collections/memberprogressallcourses.js
@@ -0,0 +1,22 @@
+$(function () {
+
+    App.Collections.memberprogressallcourses = Backbone.Collection.extend({
+
+
+        url: function () {
+            var url = App.Server + '/membercourseprogress/_design/bell/_view/GetMemberAllCourseResult?key="' + this.memberId + '"&include_docs=true'
+            return url
+        },
+
+
+        parse: function (response) {
+            var docs = _.map(response.rows, function (row) {
+                return row.doc
+            })
+            return docs
+        },
+
+        model: App.Models.membercourseprogress,
+    })
+
+})
