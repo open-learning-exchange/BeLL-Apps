c8c35623896c00a4319d41b6442f683f7880635b
unknown <omer.yousaf@pc-247.Cdocs.local>
2014-04-30 12:36:37 +0500

    adding dynamically displaying rows in logging report template

--- attachments/MyApp/app/Router.js
+++ attachments/MyApp/app/Router.js
@@ -2288,27 +2288,27 @@ var test=new App.Models.CourseInvitation()
   "Most_Freq_Open":
   [
     {
-    "resourceName":"asdf",
+    "resourceName":"Hungry Caterpillar",
     "timesOpenedCumulative":15,
     "timesOpenedByMales": 7,
     "timesOpenedByFemales": 8,
-    "avgRatingCumulative":4.5,
+    "avgRatingCumulative":4.3,
     "avgRatingByMales": 4.1,
-    "avgRatingByFemales": 4.9
+    "avgRatingByFemales": 4.5
     }, {
-    "resourceName":"asdfasf",
+    "resourceName":"Color me Physics",
     "timesOpenedCumulative":17,
     "timesOpenedByMales": 7,
     "timesOpenedByFemales": 10,
-    "avgRatingCumulative":4.5,
-    "avgRatingByMales": 4.1,
-    "avgRatingByFemales": 4.9
+    "avgRatingCumulative":3.5,
+    "avgRatingByMales": 3.1,
+    "avgRatingByFemales": 3.9
     }
   ],
   "Highest_Rated": 
   [
     {
-    "resourceName": "qwerty",
+    "resourceName": "Aesop's Fables",
     "timesOpenedCumulative":15,
     "timesOpenedByMales": 7,
     "timesOpenedByFemales": 8,
@@ -2320,7 +2320,7 @@ var test=new App.Models.CourseInvitation()
   "Lowest_Rated": 
   [
     {
-    "resourceName": "lkjh",
+    "resourceName": "Snow White and the 7 Dwarfs",
     "timesOpenedCumulative":150,
     "timesOpenedByMales": 70,
     "timesOpenedByFemales": 80,
