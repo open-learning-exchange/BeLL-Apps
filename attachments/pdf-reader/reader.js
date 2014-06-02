f9ad010efdfe1898047a99ea598511e24ad3ba76
zmehmood <zagham.mehmood@northbaysolutions.net>
2014-06-02 17:38:27 +0500

    -integration of bell reader

--- attachments/pdf-reader/reader.js
+++ attachments/pdf-reader/reader.js
@@ -1,31 +1,41 @@
 	(function($) {
 
 		var url = $.url()
+	    
+	    var file=url.param("doc")
+	    var docId=file
+	    
+	    if(file.indexOf('/')!=-1){
+	      var vars=file.split('/')
+	          docId=vars[2]
+	    }	    
+        
+        var dbName='resources'
+			
 		
-		console.log()
 		// @todo If there is not doc defined, show a dialog for giving us a doc ID.
 
-		//	
 		// Get the attached pages from the document in couchdb
-		//
-		var doc = $.couch.db(url.segment(1)).openDoc(url.param("doc"), {
+
+		var doc = $.couch.db(dbName).openDoc(docId, {
               success: function(doc) {
 				$(".page-number").click(function() {
 					$(".go-to").slideDown()
 					return false
 				})
-
-				//
+				
 				// Set the "go to library" URL
-				//
-				$(".to-library").attr('href', "/" + url.segment(1) + "/_design/library/app.html#page-submit-feedback&id=" + url.segment(2))
+				
+				$(".to-library").attr('href', "/" +  url.segment(1) + "/_design/bell/MyApp/index.html#resources")
 
         		var pages = [] 
 				// Only put files in the pages directory into the pages array
 				$.each(doc._attachments, function (key, value) {
-					if(key.indexOf("pages/") === 0) {
-						pages.push(key)
-					}
+				    //  console.log(key)
+				    pages.push(key)
+					// if(key.indexOf("pages/") === 0) {
+					//	pages.push(key)
+					// }
 				})
 				pages.sort()
 
@@ -45,19 +55,20 @@
 				// In cases where window.width < image.width we'll need to set the viewport
 				// so that it rests with the full image width in view on load.  Haven't
 				// had any luck making this work as of yet using the meta tags.
-				$(".view").hide().html('<img width="100%" src="/' + url.segment(1) + '/' + url.param("doc") + '/' + pages[thisPage] + '">').show("slide", { direction: url.param("slide") }, 500);
+				
+				
+				$(".view").hide().html('<img width="100%" src="/' + dbName + '/' + docId + '/' + pages[thisPage] + '">').show("slide", { direction: url.param("slide") }, 500);
 				setTimeout(function() {
 					var newHeight = $(".view img").height()
 					$(".view").height(newHeight)
 				}, 600)
 
-
-				//
 				// Display the page number
-				//
+				
 				var readPage = thisPage + 1
 
 				// We may have an offset that we want to take into account
+				
 				if (doc.start_page) {
 					if(doc.start_page > readPage) {
 						// @todo Create a Roman Numeral
@@ -70,44 +81,40 @@
 				$('a.page-number').html("<div class='page'>page</div>" + readPage)
 				$('a.page-number').fadeIn()
 
-
-				// 
-				// Set up the next and previous URLs
-				//		
-
+				// Set up the next and previous URLs	
 				// Determine next and previous pages
+				
 				if(thisPage == pages.length) {
 					nextPage = 0
-				}
-				else {
+				}else {
 					nextPage = thisPage + 1
 				}
 
 				if(thisPage == 0) {
 					previousPage = pages.length
-				}
-				else {
+				}else {
 					previousPage = thisPage - 1
 				}
+				
 				// Set the URLs
+				
 				$("a.next").attr("href", url.attr("path") + "?slide=right&page=" + nextPage).click(function() {
 					$(".view img").hide("slide", { direction: "left" }, 250);
 					$(".page-number").fadeOut(300)
-					setTimeout(function(){window.location.assign(url.attr("path") + "?slide=right&page=" + nextPage + "&doc=" + url.param('doc'))},300)
+					setTimeout(function(){window.location.assign(url.attr("path") + "?slide=right&page=" + nextPage + "&doc=" + docId)},300)
 					return false
 				})
 				$("a.previous").attr("href", url.attr("path") + "?slide=left&page=" + previousPage).click(function() {
 					$(".view img").hide("slide", { direction: "right" }, 250);
 					$(".page-number").fadeOut(750)
-					setTimeout(function(){window.location.assign(url.attr("path") + "?slide=left&page=" + previousPage + "&doc=" + url.param('doc'))},300)
+					setTimeout(function(){window.location.assign(url.attr("path") + "?slide=left&page=" + previousPage + "&doc=" + docId)},300)
 					return false
 				})
 
-				//
 				// Cache the next and previous page
-				//
-				$(".cache").append('<img width="100%" src="/' + url.segment(1) + '/' + url.param("doc") + '/' + pages[previousPage] + '">');
-				$(".cache").append('<img width="100%" src="/' + url.segment(1) + '/' + url.param("doc") + '/' + pages[nextPage] + '">');
+				
+				$(".cache").append('<img width="100%" src="/' + dbName+ '/' + docId + '/' + pages[previousPage] + '">');
+				$(".cache").append('<img width="100%" src="/' + dbName + '/' + docId + '/' + pages[nextPage] + '">');
 
 
 			  }
