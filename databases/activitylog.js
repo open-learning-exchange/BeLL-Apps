c2a4d61203445deaddd139660b294c2516d44ad5
Adnan Khalil <mr.adnaan004@gmail.com>
2015-02-26 20:30:16 +0500

    updated design docs for Activitylog

--- databases/activitylog.js
+++ databases/activitylog.js
@@ -6,33 +6,53 @@ var couchapp = require('couchapp')
 ddoc =  { _id:'_design/bell' }
 
 ddoc.views = {
-  getDocumentByDate: {
-    map: function(doc) {
-      if(doc.logDate) {
-        var datePart = doc.logDate.match(/\d+/g),
-        month = datePart[0], // get only two digits
-        day = datePart[1], year = datePart[2];
-        var newdate=year+'/'+month+'/'+day;
-        emit(newdate, doc);
-      }
+    getDocumentByDate: {
+        map: function(doc) {
+            if(doc.logDate) {
+                var datePart = doc.logDate.match(/\d+/g),
+                month = datePart[0], // get only two digits
+                day = datePart[1], year = datePart[2];
+                var newdate=year+'/'+month+'/'+day;
+                emit(newdate, doc);
+            }
+        }
+    },
+    getdocBylogdate: {
+        map: function(doc) {
+            emit(doc.logDate, doc);
+        }
+    },
+    getDocByCommunityCode:{
+        map: function(doc){
+            if(doc && doc.logDate){
+                var datePart = doc.logDate.match(/\d+/g),
+                month = datePart[0], // get only two digits
+                day = datePart[1], year = datePart[2];
+                var newdate=year+'/'+month+'/'+day;
+                emit([doc.community, newdate], null);
+            }
+        }
+    },
+    GetMaleCountByCommunity:{
+        map: function (doc){
+            if(doc && doc.community){
+                emit(doc.community, doc.male_new_signups)
+            }
+        },
+        reduce: function(keys, values) {
+            return sum(values);
+        }
+    },
+    GetFemaleCountByCommunity:{
+        map: function (doc){
+            if(doc && doc.community){
+                emit(doc.community, doc.female_new_signups)
+            }
+        },
+        reduce: function(keys, values) {
+            return sum(values);
+        }
     }
-  },
-  getdocBylogdate: {
-    map: function(doc) {
-      emit(doc.logDate, doc);
-    }
-  },
-  getDocByCommunityCode:{
-      map: function(doc){
-          if(doc && doc.logDate){
-              var datePart = doc.logDate.match(/\d+/g),
-              month = datePart[0], // get only two digits
-              day = datePart[1], year = datePart[2];
-              var newdate=year+'/'+month+'/'+day;
-              emit([doc.community, newdate], null);
-          }
-      }
-  }
 }
 
 module.exports = ddoc;
