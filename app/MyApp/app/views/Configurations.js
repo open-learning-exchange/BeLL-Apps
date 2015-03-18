247404a52415c3582579213be76c3b52bb7e1d53
Adnan Khalil <mr.adnaan004@gmail.com>
2015-03-18 18:56:55 +0500

    First day ActivityLog logging dummy communityCode Issue Resolved

--- app/MyApp/app/views/Configurations.js
+++ app/MyApp/app/views/Configurations.js
@@ -16,6 +16,14 @@ $(function () {
             this.$el.append(this.form.render().el);
             this.$el.append('<a style="margin-left:2px" class="btn btn-success" id="formButton">Submit Configurations </a>');
         },
+        getFormattedDate:function(date) {
+            var year = date.getFullYear();
+            var month = (1 + date.getMonth()).toString();
+            month = month.length > 1 ? month : '0' + month;
+            var day = date.getDate().toString();
+            day = day.length > 1 ? day : '0' + day;
+            return  month + '/' + day + '/' + year;
+        },
         setForm:function(){
             this.form.commit();
             if (this.form.validate() != null) {
@@ -32,13 +40,43 @@ $(function () {
             con.set('type',Config.get('type'));
             con.set('notes',Config.get('notes'));
             con.save(null,{ success: function(doc,rev){
+
                 App.configuration = con;
+
+                // Get Current Date
+                var currentdate = new Date();
+                var year = currentdate.getFullYear();
+                var month = (1 + currentdate.getMonth()).toString();
+                month = month.length > 1 ? month : '0' + month;
+                var day = currentdate.getDate().toString();
+                day = day.length > 1 ? day : '0' + day;
+                var logcurrentdate = year + '/' + month + '/' + day;
+
+                $.ajax({
+                    type: 'GET',
+                    url: '/activitylog/_design/bell/_view/getDocumentByDate?key="'+ logcurrentdate +'"',
+                    dataType: 'json',
+                    success: function (response) {
+                        var logModel = response.rows[0].value;
+                        logModel.community = App.configuration.get("code");
+
+                        //Now Posting the Updated Activitylog Model
+                        $.ajax({
+                            type: 'PUT',
+                            url: '/activitylog/'+ logModel._id +'/?rev=' + logModel._rev,
+                            data: JSON.stringify(logModel),
+                            async: false,
+                            dataType: 'json',
+                            success: function (response) {
+                                console.log(response);
+                            }
+                        });
+                    }
+                });
+
                 alert('Configurations are Successfully Added');
                 Backbone.history.navigate('dashboard', {trigger: true});
             }});
-    	    	
-
-        
         }
 
     })
