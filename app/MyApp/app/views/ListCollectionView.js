52906c98b63f764329c4229fcc7993319554e486
Saba Baig <saba.baig@northbaysolutions.net>
2015-09-18 09:54:37 +0500

    Issue#41 Code Clean Up

--- app/MyApp/app/views/ListCollectionView.js
+++ app/MyApp/app/views/ListCollectionView.js
@@ -1,175 +1,174 @@
 $(function() {
 
-	App.Views.ListCollectionView = Backbone.View.extend({
+    App.Views.ListCollectionView = Backbone.View.extend({
 
-		id: "invitationForm",
+        id: "invitationForm",
 
-		events: {
-			"click #formButton": "setForm",
-			"submit form": "setFormFromEnterKey",
-			"click #cancelButton": "hidediv",
-			"click #deleteButton": "deleteRecord",
-			"click #invitationForm .bbf-form .field-IsMajor input": "nesttedHideShow"
-
-		},
-		nesttedHideShow: function(e) {
-			if ($("#invitationForm .bbf-form .field-IsMajor input").is(':checked')) {
-				$("#invitationForm .bbf-form .field-NesttedUnder").css('visibility', 'hidden')
-			} else {
-				$("#invitationForm .bbf-form .field-NesttedUnder").css('visibility', 'visible')
-			}
-		},
-		hidediv: function() {
-			$('#invitationdiv').fadeOut(1000)
-			document.getElementById('cont').style.opacity = 1.0
-			document.getElementById('nav').style.opacity = 1.0
-			setTimeout(function() {
-				$('#invitationdiv').hide()
-			}, 1000);
-		},
-        deleteCollectionNameFromResources : function(idOfCollection){
-            alert("Calling AJAX");
-            $.ajax({
-                    url:'/resources/_design/bell/_view/resourceOnTag?_include_docs=true&key="' + idOfCollection + '"',
-                      
-                    type: 'GET',
-                    dataType: 'json',
-                    success: function (resResult) {
-                        var result = resResult.rows;
-                        var tempResult = [];
-                        for (var i = 0; i < result.length; i++) {
-                            if(result[i].value.Tag.length>0) {
-                                var index=result[i].value.Tag.indexOf(idOfCollection);
-                                if(index>-1)
-                                {
-                                    result[i].value.Tag.splice(index, 1);
-                                }
-                            }
-                          //  result[i].doc.sum = 0;
-                            //result[i].doc.timesRated = 0;
-                            tempResult.push(result[i].value);
-
-                        }
-                        $.couch.db('resources').bulkSave({
-                            "docs": tempResult
-                        }, {
-                            success: function (data) {
-                                alert("Successfully pushed resources back");
-                            }
-                    });
-                    }
-                });
+        events: {
+            "click #formButton": "setForm",
+            "submit form": "setFormFromEnterKey",
+            "click #cancelButton": "hidediv",
+            "click #deleteButton": "deleteRecord",
+            "click #invitationForm .bbf-form .field-IsMajor input": "nesttedHideShow"
 
         },
-		deleteRecord: function() {
-			$('.form .field-Tag select option[value=' + this.model.get("_id") + "]").remove();
-			$('#' + this.model.get("_id")).parent('tr').remove();
+        nesttedHideShow: function(e) {
+            if ($("#invitationForm .bbf-form .field-IsMajor input").is(':checked')) {
+                $("#invitationForm .bbf-form .field-NesttedUnder").css('visibility', 'hidden')
+            } else {
+                $("#invitationForm .bbf-form .field-NesttedUnder").css('visibility', 'visible')
+            }
+        },
+        hidediv: function() {
+            $('#invitationdiv').fadeOut(1000)
+            document.getElementById('cont').style.opacity = 1.0
+            document.getElementById('nav').style.opacity = 1.0
+            setTimeout(function() {
+                $('#invitationdiv').hide()
+            }, 1000);
+        },
+        deleteCollectionNameFromResources: function(idOfCollection) {
+
+            $.ajax({
+                url: '/resources/_design/bell/_view/resourceOnTag?_include_docs=true&key="' + idOfCollection + '"',
+
+                type: 'GET',
+                dataType: 'json',
+                success: function(resResult) {
+                    var result = resResult.rows;
+                    var tempResult = [];
+                    for (var i = 0; i < result.length; i++) {
+                        if (result[i].value.Tag.length > 0) {
+                            var index = result[i].value.Tag.indexOf(idOfCollection);
+                            if (index > -1) {
+                                result[i].value.Tag.splice(index, 1);
+                            }
+                        }
+                        //  result[i].doc.sum = 0;
+                        //result[i].doc.timesRated = 0;
+                        tempResult.push(result[i].value);
+
+                    }
+                    $.couch.db('resources').bulkSave({
+                        "docs": tempResult
+                    }, {
+                        success: function(data) {
+                            alert("Successfully pushed resources back");
+                        }
+                    });
+                }
+            });
+
+        },
+        deleteRecord: function() {
+            $('.form .field-Tag select option[value=' + this.model.get("_id") + "]").remove();
+            $('#' + this.model.get("_id")).parent('tr').remove();
             this.deleteCollectionNameFromResources(this.model.get("_id"));
             //Call from here method deleteCollectionNameFromResources/////////////////////////////////////
-			this.model.set({
-				'show': false
-			})
-			this.model.save({
-				success: location.reload()
-			})
-		},
-		render: function() {
-			var inviteForm = this
+            this.model.set({
+                'show': false
+            })
+            this.model.save({
+                success: location.reload()
+            })
+        },
+        render: function() {
+            var inviteForm = this
 
-			this.form = new Backbone.Form({
-				model: inviteForm.model
-			})
-			this.$el.append(this.form.render().el)
-			var $button = $('<a class="btn btn-success" id="formButton">Save</button>')
-			this.$el.append($button)
-			this.$el.append('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;')
-			this.$el.append('<a class="btn btn-warning" id="cancelButton">Cancel</button>')
-			if (this.model.get('_id') != undefined) {
-				this.$el.append('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;')
-				this.$el.append('<a class="btn btn-danger" id="deleteButton">Delete</button>')
-			}
-		},
+            this.form = new Backbone.Form({
+                model: inviteForm.model
+            })
+            this.$el.append(this.form.render().el)
+            var $button = $('<a class="btn btn-success" id="formButton">Save</button>')
+            this.$el.append($button)
+            this.$el.append('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;')
+            this.$el.append('<a class="btn btn-warning" id="cancelButton">Cancel</button>')
+            if (this.model.get('_id') != undefined) {
+                this.$el.append('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;')
+                this.$el.append('<a class="btn btn-danger" id="deleteButton">Delete</button>')
+            }
+        },
 
-		setFormFromEnterKey: function(event) {
-			event.preventDefault()
-			this.setForm()
-		},
-		setForm: function() {
+        setFormFromEnterKey: function(event) {
+            event.preventDefault()
+            this.setForm()
+        },
+        setForm: function() {
 
-			// Put the form's input into the model in memory
-			this.form.commit()
-			var newTitle = this.model.get("CollectionName")
-			var titleMatch = false
-			var allres = new App.Collections.listRCollection()
-			allres.fetch({
-				async: false
-			})
-			allres.each(function(m) {
+            // Put the form's input into the model in memory
+            this.form.commit()
+            var newTitle = this.model.get("CollectionName")
+            var titleMatch = false
+            var allres = new App.Collections.listRCollection()
+            allres.fetch({
+                async: false
+            })
+            allres.each(function(m) {
 
-				if (m.get("show") == true && newTitle == m.get("CollectionName")) {
-					titleMatch = true
-				}
-			})
-			if (titleMatch) {
-				alert("Collection Name already exist")
-				$('#invitationdiv').fadeOut(1000)
+                if (m.get("show") == true && newTitle == m.get("CollectionName")) {
+                    titleMatch = true
+                }
+            })
+            if (titleMatch) {
+                alert("Collection Name already exist")
+                $('#invitationdiv').fadeOut(1000)
 
-				document.getElementById('cont').style.opacity = 1.0
-				document.getElementById('nav').style.opacity = 1.0
-			} else {
-				if (this.model.get('NesttedUnder') != '--Select--') {
-					this.model.set({
-						'IsMajor': false
-					})
-				} else {
-					this.model.set({
-						'IsMajor': true
-					})
-				}
-				if (this.model.get('CollectionName').length > 0) {
+                document.getElementById('cont').style.opacity = 1.0
+                document.getElementById('nav').style.opacity = 1.0
+            } else {
+                if (this.model.get('NesttedUnder') != '--Select--') {
+                    this.model.set({
+                        'IsMajor': false
+                    })
+                } else {
+                    this.model.set({
+                        'IsMajor': true
+                    })
+                }
+                if (this.model.get('CollectionName').length > 0) {
 
-					var that = this
+                    var that = this
 
-					this.model.save(null, {
-						success: function(m) {
-							alert("Collection Saved Successfully")
-							if (that.model.get('_id') == undefined) {
-								if (that.model.get('NesttedUnder') == '--Select--') {
-									if (that.model.get('IsMajor') == true) {
-										$('.form .field-Tag select').append('<option class="MajorCategory" value="' + that.model.get('id') + '">' + that.model.get('CollectionName') + '</option>')
-									} else
-										$('.form .field-Tag select').append('<option value="' + that.model.get('id') + '">' + that.model.get('CollectionName') + '</option>')
-
-
-								} else {
-									if ($('.form .field-Tag select option[value=' + that.model.get("NesttedUnder") + "]") != null) {
-										$('.form .field-Tag select option[value=' + that.model.get("NesttedUnder") + "]").after('<option  value="' + that.model.get('id') + '">' + that.model.get('CollectionName') + '</option>');
-									}
-								}
-								$('#invitationdiv').fadeOut(1000)
-
-								document.getElementById('cont').style.opacity = 1.0
-								document.getElementById('nav').style.opacity = 1.0
-								setTimeout(function() {
-									$('#invitationdiv').hide()
-								}, 1000);
-
-								$('.form .field-Tag select').multiselect('refresh')
-							} else {
-								location.reload()
-							}
-						}
-					})
-
-				} else {
-					alert("Enter collection name!")
-				}
-
-			}
+                    this.model.save(null, {
+                        success: function(m) {
+                            alert("Collection Saved Successfully")
+                            if (that.model.get('_id') == undefined) {
+                                if (that.model.get('NesttedUnder') == '--Select--') {
+                                    if (that.model.get('IsMajor') == true) {
+                                        $('.form .field-Tag select').append('<option class="MajorCategory" value="' + that.model.get('id') + '">' + that.model.get('CollectionName') + '</option>')
+                                    } else
+                                        $('.form .field-Tag select').append('<option value="' + that.model.get('id') + '">' + that.model.get('CollectionName') + '</option>')
 
 
-		},
+                                } else {
+                                    if ($('.form .field-Tag select option[value=' + that.model.get("NesttedUnder") + "]") != null) {
+                                        $('.form .field-Tag select option[value=' + that.model.get("NesttedUnder") + "]").after('<option  value="' + that.model.get('id') + '">' + that.model.get('CollectionName') + '</option>');
+                                    }
+                                }
+                                $('#invitationdiv').fadeOut(1000)
 
-	})
+                                document.getElementById('cont').style.opacity = 1.0
+                                document.getElementById('nav').style.opacity = 1.0
+                                setTimeout(function() {
+                                    $('#invitationdiv').hide()
+                                }, 1000);
+
+                                $('.form .field-Tag select').multiselect('refresh')
+                            } else {
+                                location.reload()
+                            }
+                        }
+                    })
+
+                } else {
+                    alert("Enter collection name!")
+                }
+
+            }
+
+
+        },
+
+    })
 
 })
