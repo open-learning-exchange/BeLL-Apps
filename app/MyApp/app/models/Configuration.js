a2c587fbbd442f2325a22e4d7f651411672ccf5b
MWakas <wksg555@gmail.com>
2015-10-07 09:49:55 +0500

    Issue#112: The current langauge field has been hidden.

--- app/MyApp/app/models/Configuration.js
+++ app/MyApp/app/models/Configuration.js
@@ -13,6 +13,11 @@ $(function() {
 
             return url
         },
+        defaults: {
+            currentLanguage: {
+                type: 'Text'
+            }
+        },
         schema: {
             name: {
                 type: 'Text',
@@ -47,9 +52,6 @@ $(function() {
                 options: ["Select an Option","Arabic", "Asante", "Chinese", "English", "Ewe", "French", "Hindi", "Kyrgyzstani", "Nepali", "Portuguese", "Punjabi", "Russian",
                     "Somali", "Spanish", "Swahili", "Urdu"
                 ]
-            },
-            currentLanguage: {
-                type: 'Text'
             }
         }
     })
