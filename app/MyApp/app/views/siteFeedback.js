debe4eb618c884bd0f0ce4c2a24377899b675ffb
SabaBaig <saba.baig@northbaysolutions.net>
2015-12-10 12:02:43 +0500

    Issue#103(Feedback page on community is proviging MUI support)

--- app/MyApp/app/views/siteFeedback.js
+++ app/MyApp/app/views/siteFeedback.js
@@ -61,9 +61,9 @@ $(function() {
 
         render: function() {
             this.$el.append('<br/><br/><div class="form-field" ><input name="PageUrl" id="PageUrl" type="text"></div>')
-            this.$el.append('<div class="form-field" style="margin-left:23px;"><input name="priority" value="urgent" id="priority" type="checkbox"><label for="priority">urgent</label></div>')
-            this.$el.append('<div class="form-field" style="margin-top: -19px;margin-left: 115px;"> <input type="radio" name="category" value="Bug">&nbsp Bug &nbsp&nbsp&nbsp<input type="radio" name="category" value="Question">&nbsp Question &nbsp&nbsp&nbsp<input type="radio" name="category" value="Suggestion">&nbsp Suggestion &nbsp&nbsp&nbsp</div><br/><br/>')
-            this.$el.append('<div class="form-field" style="margin-left:23px;"><textarea †rows="7" type="text" name="comment" placeholder="Give us your feedback on the current page ... " id="comment"></textarea></div>')
+            this.$el.append('<div class="form-field" style="margin-left:23px;"><input name="priority" value="urgent" id="priority" type="checkbox"><label for="priority">'+App.languageDict.attributes.Urgent+'</label></div>')
+            this.$el.append('<div class="form-field" style="margin-top: -19px;margin-left: 115px;"> <input type="radio" name="category" value="Bug">&nbsp '+App.languageDict.attributes.Bug+' &nbsp&nbsp&nbsp<input type="radio" name="category" value="Question">&nbsp '+App.languageDict.attributes.Question+' &nbsp&nbsp&nbsp<input type="radio" name="category" value="Suggestion">&nbsp '+App.languageDict.attributes.Suggestion+' &nbsp&nbsp&nbsp</div><br/><br/>')
+            this.$el.append('<div class="form-field" style="margin-left:23px;"><textarea †rows="7" type="text" name="comment" placeholder="" id="comment"></textarea></div>')
             this.$el.append('<div class="form-field"><input name="Resolved" id="Resolved" type="text"></div>')
             this.$el.append('<div class="form-field"><input name="memberLogin" id="memberLogin" type="text"></div>')
             this.$el.append('<div class="form-field"><input name="time" id="time" type="text"></div>')
@@ -71,7 +71,7 @@ $(function() {
             $('#Resolved', this.$el).hide()
             $('#memberLogin', this.$el).hide()
             $('#time', this.$el).hide()
-            var $button = $('<br/><div id="f-formButton"><button class="btn btn-hg btn-danger" id="CancelButton">Cancel</button><button class="btn btn-hg btn-info" id="ViewAllButton">View</button><button class="btn btn-hg btn-primary" id="formButton">Submit</button></div>')
+            var $button = $('<br/><div id="f-formButton"><button class="btn btn-hg btn-danger" id="CancelButton">'+App.languageDict.attributes.Cancel+'</button><button class="btn btn-hg btn-info" id="ViewAllButton">'+App.languageDict.attributes.View+'</button><button class="btn btn-hg btn-primary" id="formButton">'+App.languageDict.attributes.Submit+'</button></div>')
             this.$el.append($button)
         }
     })
