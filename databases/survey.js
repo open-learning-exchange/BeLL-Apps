2b6603fcf55a7fa8f98659a42b5d4ebbf60a698b
MWakas <wksg555@gmail.com>
2015-12-17 14:45:18 +0500

    Issue#111: Survey db has been added.

---
+++ databases/survey.js
@@ -0,0 +1,18 @@
+var couchapp = require('couchapp'),
+    path = require('path');
+
+ddoc = {
+    _id: '_design/bell'
+}
+
+ddoc.views = {
+    surveyById: {
+        map: function(doc) {
+            if (doc._id) {
+                emit(doc._id, doc);
+            }
+        }
+    }
+}
+
+module.exports = ddoc;
