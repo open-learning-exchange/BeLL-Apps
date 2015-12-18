d49e6b35d1aa686ebcf5df21e9018c29927016a7
MWakas <wksg555@gmail.com>
2015-12-18 16:20:32 +0500

    Issue#111: Survey schema has been added for nation

---
+++ app/nation/app/models/Survey.js
@@ -0,0 +1,29 @@
+$(function() {
+
+    App.Models.Survey = Backbone.Model.extend({
+
+        idAttribute: "_id",
+
+        url: function() {
+            if (_.has(this, 'id')) {
+                var url = (_.has(this.toJSON(), '_rev')) ? App.Server + '/survey/' + this.id + '?rev=' + this.get('_rev') // For UPDATE and DELETE
+                    : App.Server + '/survey/' + this.id // For READ
+            } else {
+                var url = App.Server + '/survey' // for CREATE
+            }
+            return url
+        },
+
+        defaults: {
+            kind: 'survey',
+            sentTo: [],
+            submittedBy: []
+        },
+
+        schema: {
+            Date: 'Text',
+            SurveyNo: 'Number',
+        },
+
+    })
+})
