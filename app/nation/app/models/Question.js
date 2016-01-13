fe153c968159f577420fe5f528f6d24f321f6650
MWakas <wksg555@gmail.com>
2016-01-13 12:45:01 +0500

    Issue#111: Questions schema has been added

---
+++ app/nation/app/models/Question.js
@@ -0,0 +1,31 @@
+$(function() {
+
+    App.Models.Question = Backbone.Model.extend({
+
+        idAttribute: "_id",
+
+        url: function() {
+            if (_.has(this, 'id')) {
+                var url = (_.has(this.toJSON(), '_rev')) ? App.Server + '/surveyquestions/' + this.id + '?rev=' + this.get('_rev') // For UPDATE and DELETE
+                    : App.Server + '/surveyquestions/' + this.id // For READ
+            } else {
+                var url = App.Server + '/surveyquestions' // for CREATE
+            }
+            return url
+        },
+
+        defaults: {
+            kind: 'surveyquestions'
+        },
+
+        schema: {
+            surveyId: 'Text',
+            Type: 'Text',
+            Statement: 'Text',
+            Answer: [],
+            Options: [],
+            Ratings: []
+        },
+
+    })
+})
