ea96ef08e42ee22d2563c6924b99a530d4ba25ed
MWakas <wksg555@gmail.com>
2016-01-18 10:49:04 +0500

    Issue#111: Correction in SurveyQuestions couch db views

--- databases/surveyquestions.js
+++ databases/surveyquestions.js
@@ -8,21 +8,21 @@ ddoc = {
 ddoc.views = {
     questionsById: {
         map: function(doc) {
-            if (doc._id) {
+            if (doc._id && doc.kind == 'surveyquestions') {
                 emit(doc._id, doc);
             }
         }
     },
     questionsBySurveyId: {
         map: function(doc) {
-            if (doc.surveyId) {
+            if (doc.surveyId && doc.kind == 'surveyquestions') {
                 emit(doc.surveyId, doc);
             }
         }
     },
     questionsByType: {
         map: function(doc) {
-            if (doc.Type) {
+            if (doc.Type && doc.kind == 'surveyquestions') {
                 emit(doc.Type, doc);
             }
         }
