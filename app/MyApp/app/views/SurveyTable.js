634578299d4b31eb5a51244d1abe40785e1a47df
MWakas <wksg555@gmail.com>
2016-02-29 11:37:06 +0500

    Issue#111: SurveyRow view has been added at community level

--- app/MyApp/app/views/SurveyTable.js
+++ app/MyApp/app/views/SurveyTable.js
@@ -5,13 +5,20 @@ $(function () {
         tagName: "table",
         className: "table table-striped",
         collectionInfo:[],
-        add: function () {
-
+        add: function (model, isAlreadyDownloaded) {
+            if (isAlreadyDownloaded) {
+                this.$el.append('<tr id="' + model._id + '"><td>' + model.SurveyNo+ '</td><td>' + model.SurveyTitle+ '</td><td><a name="' +model._id +
+                '" class="openSurvey btn btn-info">Open</a></td></tr>');
+            } else {
+                this.$el.append('<tr id="' + model._id + '"><td>' + model.SurveyNo+ '</td><td>' + model.SurveyTitle+ '</td><td><a name="' +model._id +
+                '" class="openSurvey btn btn-info">Open</a><label>&nbsp&nbspNew</label></td></tr>');
+            }
         },
         events:{
             "click .openSurvey": 'openSurvey'
         },
         render: function () {
+            var that = this;
             this.$el.html('<tr><th>Survey No.</th><th>Title</th><th>Actions</th></tr>');
             var nationName = App.configuration.get('nationName');
             var nationUrl = App.configuration.get('nationUrl');
@@ -21,12 +28,39 @@ $(function () {
                 dataType: 'jsonp',
                 async:false,
                 success: function(json) {
-                    console.log(json);
                     var SurveyDocsFromNation = [];
                     _.each(json.rows, function(row) {
                         SurveyDocsFromNation.push(row);
                     });
-                    console.log(SurveyDocsFromNation);
+                    // fetch all surveys from local/community server to see how many of the surveys from nation are new ones
+                    $.ajax({
+                        url: '/survey/_design/bell/_view/surveyBySentToCommunities?_include_docs=true&key="' + App.configuration.get('name') + '"',
+                        type: 'GET',
+                        dataType: 'json',
+                        async:false,
+                        success: function(commSurdata) {
+                            var SurveyDocsFromComm = [];
+                            _.each(commSurdata.rows, function(row) {
+                                SurveyDocsFromComm.push(row);
+                            });
+                            _.each(SurveyDocsFromNation,function(row){
+                                var surveyFromNation = row.value;
+                                var index = SurveyDocsFromComm.map(function(element) {
+                                    return element.value._id;
+                                }).indexOf(surveyFromNation._id);
+                                var isAlreadyDownloaded = false;
+                                if (index > -1) { // its a new or yet-to-be-download survey from nation, so display it as new
+                                    isAlreadyDownloaded = true;
+                                    that.add(surveyFromNation, isAlreadyDownloaded);
+                                } else { // its an already downloaded survey. display it without the new mark
+                                    that.add(surveyFromNation, isAlreadyDownloaded);
+                                }
+                            });
+                        },
+                        error: function(status) {
+                            console.log(status);
+                        }
+                    });
                 },
                 error: function(status) {
                     console.log(status);
@@ -35,7 +69,7 @@ $(function () {
             applyStylingSheet();
         },
         openSurvey:function(e){
-
+            alert("This functionality is under-construction");
         }
     })
 })
