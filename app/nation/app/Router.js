2e7aefc6b3d03efb18cf0c6aee7d6ed65fba743f
MWakas <wksg555@gmail.com>
2016-04-01 11:49:05 +0500

    Issue#202: Initial functionality of donwloading multiple survey responses in CSV

--- app/nation/app/Router.js
+++ app/nation/app/Router.js
@@ -2957,6 +2957,76 @@ $(function() {
             });
         },
 
+        downloadCommunitySurveys: function (communityNames) {
+            if(communityNames.length > 0) {
+                communityNames = communityNames.split(',');
+                $.ajax({
+                    url: '/surveyresponse/_all_docs?include_docs=true',
+                    type: 'GET',
+                    dataType: "json",
+                    async: false,
+                    success: function (json) {
+                        console.log(json);
+                    },
+                    error: function (err) {
+                        console.log(err);
+                    }
+                });
+            }
+        },
+
+        JSONToCSVConvertor: function (JSONData, ReportTitle, label) {
+            //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
+            var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+            var CSV = '';
+            //Set Report title in first row or line
+            CSV += label + '\r\n\n';
+            //This will generate the Label/Header
+            var row = "";
+            //This loop will extract the label from 1st index of on array
+            for (var index in arrData[0]) {
+                //Now convert each value to string and comma-seprated
+                row += index + ',';
+            }
+            row = row.slice(0, -1);
+            //append Label row with line break
+            CSV += row + '\r\n';
+            //1st loop is to extract each row
+            for (var i = 0; i < arrData.length; i++) {
+                var row = "";
+                //2nd loop will extract each column and convert it in string comma-seprated
+                for (var index in arrData[i]) {
+                    row += '"' + arrData[i][index] + '",';
+                }
+                row.slice(0, row.length - 1);
+                //add a line break after each row
+                CSV += row + '\r\n';
+            }
+            if (CSV == '') {
+                alert("Invalid data");
+                return;
+            }
+            //Generate a file name
+            var fileName = "";
+            //this will remove the blank-spaces from the title and replace it with an underscore
+            fileName += ReportTitle.toString().replace(/ /g,"_");
+            //Initialize file format you want csv or xls
+            var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
+            // Now the little tricky part.
+            // you can use either>> window.open(uri);
+            // but this will not work in some browsers
+            // or you will not get the correct file extension
+            //this trick will generate a temp <a /> tag
+            var link = document.createElement("a");
+            link.href = uri;
+            //set the visibility hidden so it will not effect on your web-layout
+            link.style = "visibility:hidden";
+            link.download = fileName + ".csv";
+            //this part will append the anchor tag and remove it after automatic click
+            this.$el.append(link);
+            link.click();
+        },
+
         underConstruction: function() {
             App.$el.children('.body').html('<div  id="underConstruction" style="margin:0 auto"><h4>This Functionality is under construction.</h4></div>')
         },
