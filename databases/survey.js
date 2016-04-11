017f67482354697aac410db7a1eeed2b5125ae73
MWakas <wksg555@gmail.com>
2016-04-11 16:40:23 +0500

    Issue#217: CouchDB view has been added in survey's db

--- databases/survey.js
+++ databases/survey.js
@@ -42,6 +42,21 @@ ddoc.views = {
             }
         }
     },
+    surveyByreceiverIds: {
+        map: function(doc) {
+            if (doc.receiverIds && doc.kind == 'survey') {
+                if (Array.isArray(doc.receiverIds)) {
+                    if (doc.receiverIds.length > 0) {
+                        for (var idx in doc.receiverIds) {
+                            emit(doc.receiverIds[idx], doc);
+                        }
+                    }
+                } else {
+                    emit(doc.receiverIds, doc)
+                }
+            }
+        }
+    },
     allSurveys: {
         map: function(doc) {
             if (doc.SurveyNo  && doc.kind == 'survey'){
