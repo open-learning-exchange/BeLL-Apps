849dad45251a35901acb67a7aca27e7e72f03d49
MWakas <wksg555@gmail.com>
2016-04-13 12:25:29 +0500

    Issue#217: Fetching member's data of survey and surveyresponse

--- app/MyApp/app/views/SurveyTableForMembers.js
+++ app/MyApp/app/views/SurveyTableForMembers.js
@@ -10,6 +10,43 @@ $(function () {
         },
         render: function () {
             this.$el.html('<tr><th>Survey No.</th><th>Title</th><th>Actions</th></tr>');
+            var members = new App.Collections.Members()
+            var member, memberId;
+            members.login = $.cookie('Member.login');
+            members.fetch({
+                success: function () {
+                    if (members.length > 0) {
+                        member = members.first();
+                        memberId = member.get('login') + '_' + member.get('community');
+                        $.ajax({
+                            url: '/survey/_design/bell/_view/surveyByreceiverIds?_include_docs=true&key="' + memberId + '"',
+                            type: 'GET',
+                            dataType: 'json',
+                            async:false,
+                            success: function(memberSurveyData) {
+                                console.log(memberSurveyData);
+                                $.ajax({
+                                    url: '/surveyresponse/_design/bell/_view/surveyResByreceiverIds?_include_docs=true&key="' + memberId + '"',
+                                    type: 'GET',
+                                    dataType: 'json',
+                                    async:false,
+                                    success: function(memberSurveyResData) {
+                                        console.log(memberSurveyResData);
+                                    },
+                                    error: function(status) {
+                                        console.log(status);
+                                    }
+                                });
+                            },
+                            error: function(status) {
+                                console.log(status);
+                            }
+                        });
+                    }
+                },
+                async:false
+
+            });
             applyStylingSheet();
         },
 
