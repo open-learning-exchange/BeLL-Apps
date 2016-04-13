ceb9015e3b541d6aba7391c0d735769d20bb8863
MWakas <wksg555@gmail.com>
2016-04-13 12:24:45 +0500

    Issue#217: Couchdb view has been added for receiverIds in surveyresponse

--- databases/surveyresponse.js
+++ databases/surveyresponse.js
@@ -55,6 +55,21 @@ ddoc.views = {
                 }
             }
         }
+    },
+    surveyResByreceiverIds: {
+        map: function(doc) {
+            if (doc.receiverIds && doc.kind == 'survey') {
+                if (Array.isArray(doc.receiverIds)) {
+                    if (doc.receiverIds.length > 0) {
+                        for (var idx in doc.receiverIds) {
+                            emit(doc.receiverIds[idx], doc);
+                        }
+                    }
+                } else {
+                    emit(doc.receiverIds, doc)
+                }
+            }
+        }
     }
 
 }
