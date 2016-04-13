831ae5e245020a199d290db4597c92573be120af
MWakas <wksg555@gmail.com>
2016-04-13 13:01:22 +0500

    Issue#217: Showing the list of surveys for member

--- app/MyApp/app/views/SurveyTableForMembers.js
+++ app/MyApp/app/views/SurveyTableForMembers.js
@@ -8,7 +8,21 @@ $(function () {
         events:{
 
         },
+
+        add: function (model, isSubmitted) {
+            if (isSubmitted) {
+                this.$el.append('<tr id="' + model._id + '"><td>' + model.SurveyNo+ '</td><td>' + model.SurveyTitle+ '</td><td><a name="' +model._id +
+                    '" class="openSurvey btn btn-info" href="#openSurvey/' + model._id + '/' + isSubmitted +
+                    '">Open</a><label>&nbsp&nbspSubmitted</label></td></tr>');
+            } else {
+                this.$el.append('<tr id="' + model._id + '"><td>' + model.SurveyNo+ '</td><td>' + model.SurveyTitle+ '</td><td><a name="' +model._id +
+                    '" class="openSurvey btn btn-info" href="#openSurvey/' + model._id + '/' + isSubmitted +
+                    '">Open</a><label>&nbsp&nbspUn-Submitted</label></td></tr>');
+            }
+        },
+
         render: function () {
+            var that = this;
             this.$el.html('<tr><th>Survey No.</th><th>Title</th><th>Actions</th></tr>');
             var members = new App.Collections.Members()
             var member, memberId;
@@ -25,6 +39,10 @@ $(function () {
                             async:false,
                             success: function(memberSurveyData) {
                                 console.log(memberSurveyData);
+                                var surveyDocs = [];
+                                _.each(memberSurveyData.rows, function(row) {
+                                    surveyDocs.push(row);
+                                });
                                 $.ajax({
                                     url: '/surveyresponse/_design/bell/_view/surveyResByreceiverIds?_include_docs=true&key="' + memberId + '"',
                                     type: 'GET',
@@ -32,6 +50,25 @@ $(function () {
                                     async:false,
                                     success: function(memberSurveyResData) {
                                         console.log(memberSurveyResData);
+                                        var surveyResDocs = [];
+                                        _.each(memberSurveyResData.rows, function(row) {
+                                            if(row.value.answersToQuestions.length > 0) {
+                                                surveyResDocs.push(row);
+                                            }
+                                        });
+                                        _.each(surveyDocs,function(row){
+                                            var surveyDoc  = row.value;
+                                            var index = surveyResDocs.map(function(element) {
+                                                return element.value.SurveyNo;
+                                            }).indexOf(surveyDoc.SurveyNo);
+                                            var isSubmitted = false;
+                                            if (index == -1) { // its a survey which is not submitted yet
+                                                that.add(surveyDoc, isSubmitted);
+                                            } else { // its an already submitted survey.
+                                                isSubmitted = true;
+                                                that.add(surveyDoc, isSubmitted);
+                                            }
+                                        });
                                     },
                                     error: function(status) {
                                         console.log(status);
