92e3e1ca54b4a624ad367d550ec409f476aaa760
MWakas <wksg555@gmail.com>
2016-04-20 12:20:51 +0500

    Issue#217: Sending survey based on roles of members

--- app/nation/app/Router.js
+++ app/nation/app/Router.js
@@ -3589,6 +3589,7 @@ $(function() {
             $('#invitationdiv').append(inviteForm.el);
             $('#genderSelect').append("<option value='Male'>Male</option><option value='Female'>Female</option>");
             $('#ageGroupSelect').append("<option value='5-14'>Less than 15</option><option value='15-24'>15-24</option><option value='25-44'>25-44</option><option value='45-64'>45-64</option><option value='65-100'>65+</option>");
+            $('#rolesSelect').append("<option value='Learner'>Learner</option><option value='Leader'>Leader</option><option value='Manager'>Manager</option>");
             $('#addQuestion').css('pointer-events','none');
         },
 
