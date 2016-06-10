c558933f7c483ce8f3a99fcbe2c88291f7f3a220
SabaBaig <saba.baig@northbaysolutions.net>
2016-06-10 14:30:44 +0500

    Issue#307-A:Community Details view page

---
+++ app/nation/app/collections/CommunityConfigurations.js
@@ -0,0 +1,32 @@
+$(function() {
+
+    App.Collections.CommunityConfigurations = Backbone.Collection.extend({
+
+        initialize: function(e) {
+            if (e) {
+                this.url = App.Server + '/registeredcommunities/_all_docs?include_docs=true' + '&limit=' + e.limit
+            } else {
+                this.url = App.Server + '/registeredcommunities/_all_docs?include_docs=true'
+            }
+        },
+        setUrl: function(url) {
+            this.url = url;
+        },
+        parse: function(response) {
+            var docs = _.map(response.rows, function(row) {
+                return row.doc
+            })
+            return docs
+        },
+
+
+
+        comparator: function(model) {
+            var Name = model.get('name')
+            if (Name) return Name.toLowerCase()
+        },
+        model: App.Models.CommunityConfigurations
+
+    })
+
+})
