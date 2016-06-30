7876a311d83fdc3a38f218b317a06c553f11f3d0
Sadia Rasheed <hsadia.rasheed@gmail.com>
2016-06-30 15:29:50 +0500

    Issue#341:Badges| (UI) Badges page for student

---
+++ app/MyApp/app/views/BadgesTable.js
@@ -0,0 +1,21 @@
+ /**
+ * Created by Sadia.Rasheed on 6/30/2016.
+ */
+ $(function () {
+
+     App.Views.badgesTableForLearners = Backbone.View.extend({
+         tagName: "table",
+         className: "table table-striped",
+
+         events:{
+
+         },
+         add: function () {
+
+
+         },
+         render: function () {
+             this.$el.html('<tr><th>' + 'StepNO' + '</th><th>' + 'Step Type' + '</th><th>' + 'Quiz Credits' + '</th><th>' + 'Paper Credits' + '</th><th>' + 'Status' + '</th></tr>');
+         }
+     })
+ })
