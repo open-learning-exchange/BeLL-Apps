b61028bbf786878685012eb941622a6a6d56e5c8
Sadia Rasheed <hsadia.rasheed@gmail.com>
2016-07-11 14:17:37 +0500

    #352: Badges | (UI) badges main  page for learner

--- app/MyApp/app/views/BadgesTable.js
+++ app/MyApp/app/views/BadgesTable.js
@@ -11,11 +11,39 @@
 
          },
          add: function () {
+             var badgesRow = new App.Views.BadgesRow({
+                 model: model,
+
+             })
+            // badgesRow.courseId = this.courseId
+             badgesRow.render()
+             this.$el.append(badgesRow.el);
 
 
          },
-         render: function () {
+         addAll: function() {
              this.$el.html('<tr><th>' + 'StepNO' + '</th><th>' + 'Step Type' + '</th><th>' + 'Quiz Credits' + '</th><th>' + 'Paper Credits' + '</th><th>' + 'Status' + '</th></tr>');
+            /* var courseSteps = new App.Collections.coursesteps()
+             courseSteps.courseId=this.courseId;
+             courseSteps.fetch({
+                 async: false
+             })
+             console.log(this.collection);
+             courseSteps.forEach(this.addOne, this)*/
+           /*  for(var i=0;i<this.collection.models[i].get('stepsResult').length;i++)
+             {
+                var courseSteps = new App.Models.coursesteps()
+                 {
+
+                 }
+
+            }*/
+         },
+         render: function () {
+             this.addAll();
+             //this.$el.html('<tr><th>' + 'StepNO' + '</th><th>' + 'Step Type' + '</th><th>' + 'Quiz Credits' + '</th><th>' + 'Paper Credits' + '</th><th>' + 'Status' + '</th></tr>');
+             /////////////////////////////////////////
+
          }
      })
  })
