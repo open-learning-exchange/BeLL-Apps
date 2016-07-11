1579736aa4547e6d1b7af926f6ae23404b13850d
Sadia Rasheed <hsadia.rasheed@gmail.com>
2016-07-11 14:17:37 +0500

    #352: Badges | (UI) badges main  page for learner

---
+++ app/MyApp/app/views/BadgesRow.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Sadia.Rasheed on 7/4/2016.
+ */
+$(function () {
+
+    App.Views.BadgesRow = Backbone.View.extend({
+
+        tagName: "tr",
+        roles: null,
+        events: {
+
+        },
+
+        template: $("#template-BadgesRow").html(),
+
+        initialize: function (e) {
+
+        },
+
+        render: function () {
+
+        }
+
+    })
+
+})
