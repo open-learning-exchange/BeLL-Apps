35ef55039bdd1f99d04c03f2210468a96db9d187
Sadia Rasheed <hsadia.rasheed@gmail.com>
2016-07-11 14:17:37 +0500

    #352: Badges | (UI) badges main  page for learner

--- app/MyApp/app/views/CourseCreditsMainPage.js
+++ app/MyApp/app/views/CourseCreditsMainPage.js
@@ -8,22 +8,23 @@ $(function () {
         className: "table table-striped",
 
         events:{
-            //@todo: This needs to be done that is why I have commented this for now
-            "click #detailsButton": "creditDetails",
         },
-        add: function () {
-                this.$el.append('<tr><td>' + "Course1"+ '</td><td><a class="btn btn-success" style="margin-left:10px" id="detailsButton"  >' + "Open" + '</a></td></tr>')
+        add: function (courseModel) {
+            this.$el.append('<tr><td>' + courseModel.get('CourseTitle')+ '</td><td><a class="btn btn-success" href="#badges/edit/' + courseModel.get('_id') + '" style="margin-left:10px" id="detailsButton"  >' + "Open" + '</a></td></tr>')
         },
         render: function () {
-            var that = this;
-            this.$el.html('<tr><th>' + 'Course Names' + '</th><th>' + 'Action' + '</th></tr>');
-            this.add();
-            this.add();
+
+            var courseModel = new App.Models.Group({
+                _id: this.courseId
+            })
+            courseModel.fetch({
+                async: false
+            });
+            this.add(courseModel);
 
         },
-       // @todo: This needs to be done that is why I have commented this for now
-      creditDetails: function(){
-           creditDetails();
+        addHeading: function(){
+            this.$el.html('<tr><th>' + 'Course Names' + '</th><th>' + 'Action' + '</th></tr>');
         }
 
     })
