53b6d9ed05205ecaca78f917d0a4f913651b9a4a
Sadia Rasheed <hsadia.rasheed@gmail.com>
2016-07-20 16:58:14 +0500

    Issue#344:Badges| (UI) Credits details page for Leader

---
+++ app/MyApp/app/views/CreditsTable.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Sadia.Rasheed on 7/20/2016.
+ */
+
+$(function () {
+
+    App.Views.CreditsTable = Backbone.View.extend({
+        tagName: "table",
+        className: "table table-striped",
+
+        events:{
+        },
+        addOne: function (model) {
+            var creditsRow = new App.Views.CreditsRow({
+                model: model
+            })
+            creditsRow.render()
+            this.$el.append(creditsRow.el);
+        },
+        addAll: function() {
+            this.collection.forEach(this.addOne, this)
+        },
+        render: function () {
+            this.$el.html('<tr><th>' + 'StepNO' + '</th><th>' + 'Step Type' + '</th><th>' + 'Quiz Credits' + '</th><th>' + 'Paper Credits' + '</th><th>' + 'Status' + '</th></tr>');
+            this.addAll();/////////////////////////////////////////
+        }
+    })
+})
