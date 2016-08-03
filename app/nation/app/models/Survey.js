04c1f33a9e15a54e801b3f1c6b50aa4fec3f91cd
MWakas <wksg555@gmail.com>
2016-08-03 12:41:47 +0500

    Issue#366: Added comments in Survey schema of nation

--- app/nation/app/models/Survey.js
+++ app/nation/app/models/Survey.js
@@ -15,22 +15,22 @@ $(function() {
         },
 
         defaults: {
-            kind: 'survey',
-            sentTo: [],
-            submittedBy: [],
-            questions: [],
-            answersToQuestions: [],
-            genderOfMember: '',
-            birthYearOfMember: '',
-            communityName: '',
-            memberId: '',
-            receiverIds: []
+            kind: 'survey',//Saves kind of document according to corresponding db's.Mostly used in couch db views.
+            sentTo: [],//This array attribute saves 'names' of those communities to which a particular survey has been sent from nation.It saves value of 'name' attribute from configurations of community
+            submittedBy: [],//This array attribute saves 'names' of those communities who submitted response of a particular survey to nation.It saves value of 'name' attribute from configurations of community
+            questions: [],//Ids of questions added in a survey.These ids are actually coming from surveyquestions db.
+            answersToQuestions: [],//Ids of answer docs for questions added in a survey.These ids are actually coming from surveyanswers db.
+            genderOfMember: '',//Gender of member who is submitting his/her response to a particular survey
+            birthYearOfMember: '',//BirthDate of member who is submitting his/her response to a particular survey
+            communityName: '',//Name of community to which member(who is submitting his/her response to a particular survey) belongs.It saves value of 'name' attribute from configurations of community
+            memberId: '',//This value is a resultant of member login id + value of community attribute of that member's doc.This attribute is used to differentiate b/w multiple survey response docs of same survey
+            receiverIds: []//This array attribute saves memberIds(as explained above) of those members to which survey has been sent
         },
 
         schema: {
-            Date: 'Text',
-            SurveyNo: 'Number',
-            SurveyTitle:'Text'
+            Date: 'Text',//Date when survey was added
+            SurveyNo: 'Number',//Unique identification number of a survey.
+            SurveyTitle:'Text' //Title of a survey
         }
 
     })
