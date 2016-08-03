c02083ea59d60ba0720dfe5855d1b51fd0152e78
MWakas <wksg555@gmail.com>
2016-08-03 12:21:39 +0500

    Issue#366: Added comments in DailyLog model of nation

--- app/MyApp/app/models/DailyLog.js
+++ app/MyApp/app/models/DailyLog.js
@@ -19,14 +19,14 @@ $(function() {
             "logDate": "Text", //Date of the day for which activity log doc is created.
             "female_visits": 'number',//Count of female member visits in a day
             "male_visits": 'number',//Count of male member visits in a day
-            "female_new_signups": 'number',//Count of new male sign-ups in a day
-            "male_new_signups": 'number',//Count of new female sign-ups in a day
+            "female_new_signups": 'number',//Count of new female sign-ups in a day
+            "male_new_signups": 'number',//Count of new male sign-ups in a day
             "resourcesIds": [], //Saves ids of those resources which are opened and given feedback in a day
             "female_rating": [],//Rating count against each resource opened and rated by females in a day
             "female_timesRated": [], //Count of how many times a resource has been rated by females in a day
             "male_rating": [],//Rating count against each resource opened and rated by males in a day
             "male_timesRated": [],//Count of how many times a resource has been rated by males in a day
-            "resources_opened": [],//Ids of opened resources in a day
+            "resources_opened": [],//Ids of opened resources in a day.These ids are actually coming from resources db.
             "resources_names": [], //Names of opened resources in a day
             "female_opened": [], //Ids of opened resources by females in a day
             "male_opened": [],//Ids of opened resources by males in a day
