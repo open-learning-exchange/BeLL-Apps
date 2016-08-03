04ff814edeafdbeb1000fa872e843a827d8f27ff
SabaBaig <saba.baig@northbaysolutions.net>
2016-08-03 16:58:41 +0500

    Issue#366: Added comments in Feedback Model

--- app/MyApp/app/models/Feedback.js
+++ app/MyApp/app/models/Feedback.js
@@ -3,6 +3,7 @@ $(function() {
   App.Models.Feedback = Backbone.Model.extend({
 
     idAttribute: "_id",
+    //This model refers to the feedback for any resource.
 
     url: function() {
       if (_.has(this, 'id')) {
@@ -15,15 +16,15 @@ $(function() {
     },
 
     defaults: {
-      kind: "Feedback"
+      kind: "Feedback" //Saves kind of document according to corresponding db's.Mostly used in couch db views.
     },
 
     schema: {
-      rating: 'Text',
-      comment: 'TextArea',
-      resourceId: 'Text',
-      memberId: 'Text',
-      communityCode: 'Text'
+      rating: 'Text', //Rating(saved in the form of integers from 1 to 5) given for a particular resource
+      comment: 'TextArea', //Comments given for a particular resource
+      resourceId: 'Text', //Id of that particular resource for which feedback is given. This id is coming from resources db.
+      memberId: 'Text',//Id of that member who is giving his/her feedback for a particular resource. This id is coming from members db.
+      communityCode: 'Text' //The value of 'code' attribute from community configurations
     }
 
   })
