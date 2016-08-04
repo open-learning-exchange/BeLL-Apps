685f005d081dfbd0bfd0fb6613ef0afbb1621536
SabaBaig <saba.baig@northbaysolutions.net>
2016-08-04 10:33:02 +0500

    Issue#366: Added comments in invitation models

--- app/MyApp/app/models/InviFormModel.js
+++ app/MyApp/app/models/InviFormModel.js
@@ -1,7 +1,7 @@
 $(function() {
 
     App.Models.InviFormModel = Backbone.Model.extend({
-        //This model is used for inviting members. Either for a meetup or a course.
+        //This model is used for inviting members for a course.
         schema: {
             invitationType: { //Gives user 3 options in selection criteria for invitation
                 type: 'Select',
@@ -26,7 +26,7 @@ $(function() {
                 options: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', 'Higher']
             },
             members: {
-                type: 'Checkboxes',
+                type: 'Checkboxes', //when invitationType's value is Members then list of members is displayed with a checkbox against each member to send him an invite.
                 options: null // Populate this when instantiating
             }
         }
