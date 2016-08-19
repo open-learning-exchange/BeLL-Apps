8a82230b94fabc71ec902584fdec5537869d86e4
Sadia Rasheed <hsadia.rasheed@gmail.com>
2016-08-19 15:57:55 +0500

    Issue#430:Remove 'members of a course' page from badges' old

--- app/MyApp/app/views/CreditsLeaderView.js
+++ app/MyApp/app/views/CreditsLeaderView.js
@@ -9,19 +9,26 @@ $(function () {
 
         events:{
         },
-        add: function (courseModel) {
-            this.$el.append('<tr><td>' + courseModel.get('CourseTitle')+ '</td><td><a class="btn btn-success" href="#listLearnersCredits/' + courseModel.get('_id') + '" style="margin-left:10px" id="detailsButton"  >' + "Open" + '</a></td></tr>')
+        add: function (courseModel, memberId) {
+            this.$el.append('<tr><td>' + courseModel.get('CourseTitle')+ '</td><td><a class="btn btn-success" href="#creditsDetails/' + courseModel.get('_id') + '/' + memberId+ '" style="margin-left:10px" id="detailsButton"  >' + "Open" + '</a></td></tr>')
         },
         render: function () {
-
+            var learnerIds;
             var courseModel = new App.Models.Group({
                 _id: this.courseId
             })
             courseModel.fetch({
+                success: function (groupDoc) {
+                    learnerIds = groupDoc.get('members');
+                },
                 async: false
-            });
-            this.add(courseModel);
 
+            });
+            var learnerCollection = App.Router.getLearnersList(learnerIds);
+            var member = learnerCollection.models[0]
+            var memberId = member.get('_id')
+            console.log("member id :" + memberId);
+            this.add(courseModel,memberId );
         },
         addHeading: function(){
             this.$el.html('<tr><th>' + 'Course Names' + '</th><th>' + 'Action' + '</th></tr>');
