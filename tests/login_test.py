99f20de16f104dc52f42073f42f85142e4259009
Antonella Bernobich Dean <aberdean@users.noreply.github.com>
2016-12-06 16:19:43 +0100

    pythonize + fix small bug

--- tests/login_test.py
+++ tests/login_test.py
@@ -1,7 +1,6 @@
-import ParentCase
-from ParentCase import on_platforms
-from ParentCase import browsers
-from ParentCase import ParentCase
+from base_case import on_platforms
+from base_case import browsers
+from base_case import BaseCase
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -10,27 +9,23 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 @on_platforms(browsers)
-class LoginTest(ParentCase):
+class LoginTest(BaseCase):
     def test_login(self):
         driver = self.driver
         # go to the home page
         driver.get("http://127.0.0.1:5981/apps/_design/bell/MyApp/index.html")
-        # find the login element
+        # find the login element and enter the username
         inputElement = driver.find_element_by_name("login")
-        # type in the username
         inputElement.send_keys("admin")
-        # find the password element
+        # find the password element and enter the password
         inputElement = driver.find_element_by_name("password")
-        # type in the password
         inputElement.send_keys("password")
         # submit the form
         inputElement.submit()
         # give it time to get to the next page
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "formButton")))
-        # print page
-        print(driver.current_url)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "c84_code")))
         # ensure we're logged in
-        assert driver.current_url == "http://127.0.0.1:5981/apps/_design/bell/MyApp/index.html#configuration/add"
+        self.assertEqual(driver.current_url, "http://127.0.0.1:5981/apps/_design/bell/MyApp/index.html#configuration/add")
 
 if __name__ == "__main__":
     unittest.main()
