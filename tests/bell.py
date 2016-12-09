3283e0280afb3f4e570e9341b6bdcb1861bc1f3e
Antonella Bernobich Dean <aberdean@users.noreply.github.com>
2016-12-09 10:30:42 +0100

    add logout

--- tests/bell.py
+++ tests/bell.py
@@ -1,3 +1,8 @@
+# Library of commonly used functions
+
+# we use sleep to avoid timeout errors (TODO: should find a better way)
+from time import sleep
+
 def login(driver, username, password):
     """ (object, string, string) -> NoneType
 
@@ -8,12 +13,28 @@ def login(driver, username, password):
     # go to the login page
     driver.get("http://127.0.0.1:5981/apps/_design/bell/MyApp/index.html")
     # find the login element and enter the username
-    inputElement = driver.find_element_by_name("login")
-    inputElement.send_keys(username)
+    elem = driver.find_element_by_name("login")
+    elem.send_keys(username)
     # find the password element and enter the password
-    inputElement = driver.find_element_by_name("password")
-    inputElement.send_keys(password)
+    elem = driver.find_element_by_name("password")
+    elem.send_keys(password)
     # submit the form
-    inputElement.submit()
+    elem.submit()
+    # wait 5 secs
+    sleep(5)
+    
+def logout(driver):
+    """ (object) -> NoneType
+
+    Find the logout link and click on it.
+    """
+    # wait 5 secs
+    sleep(5)
+    # find logout link and click on it
+    link = driver.find_element_by_link_text("Logout")
+    link.click()
+    # wait 5 secs
+    sleep(5)
+    
     
 
