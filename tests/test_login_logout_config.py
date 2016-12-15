95853f2d79f13172794961444d20e863f05c8497
Antonella Bernobich Dean <aberdean@users.noreply.github.com>
2016-12-15 12:24:30 +0100

    add test_login_logout_config.py, delete test_login.py, update bell.py (fixes #30) (#31)

--- tests/test_login_logout_config.py
+++ tests/test_login_logout_config.py
@@ -1,7 +1,6 @@
 import unittest
 import bell
 
-from base_case import is_travis
 from base_case import on_platforms
 from base_case import browsers
 from base_case import BaseCase
@@ -18,71 +17,71 @@ from time import sleep
 
 @on_platforms(browsers)
 class LoginTest(BaseCase):
-                         
-    def test_login_logout(self):
-        """ Test the functionality of logging in, logging out, and 
-        adding the configuration to a new nation
-        """
-        input = ("admin", "password")
-        # if remote, test first login, otherwise test second login
-        if is_travis():
-            self.login_test(*input, True)
-            self.configuration_test()
-            self.logout_test()
-            self.login_test(*input)
-        else:
-            self.login_test(*input)
-            self.logout_test()
-                        
-    @unittest.expectedFailure
-    def test_incorrect_username(self):
-        """ Test incorrect username input in login form
+
+    def test_login(self):
+        """ NoneType -> NoneType
         
-        TODO: check username in CouchDB 
-        """
-        input = ("", "password")
-        if is_travis():
-            self.login_test(*input, True)
-        else:
-            self.login_test(*input)
-        
-    @unittest.expectedFailure    
-    def test_incorrect_password(self):
-        """ Test incorrect password input in login form
-         
-        TODO: check password in CouchDB
-        """
-        input = ("admin", "")
-        if is_travis():
-            self.login_test(*input, True)
-        else:
-            self.login_test(*input)
-        
-    def login_test(self, username, password, first=False):
-        """ (string, string, boolean) -> NoneType
-        
-        Helper function testing a correct login operation
+        Test login, logout, and configuration.
+        Login, find the landing page and, if needed, fill out 
+        the configuration, ensuring it is correctly accepted.
+        Then, ensure correct log out.
         """
         driver = self.driver
-        url = "http://127.0.0.1:5981/apps/_design/bell/MyApp/index.html"
-        if first:
-            id = "c84_code"
-            expected = url + "#configuration/add"
-        else:
-            id = "dashboard"
-            expected = url + "#dashboard"
+        
         # login
-        bell.login(driver, username, password)
-        # wait for the next page
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, id)))
-        # ensure it is the correct page
-        actual = driver.current_url
+        bell.login_test(self.driver, "admin", "password")
+        
+        # wait for the next page, and fill the configuration only if needed
+        try:
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "dashboard")))
+            self.logout_test()
+        except:
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "c84_code")))
+            self.configuration_test()
+            self.logout_test()
+                        
+    def test_incorrect_login(self):
+        """ NoneType -> NoneType
+        
+        Test incorrect username or password in login form.
+        
+        TODO: check username and password in CouchDB 
+        """
+        # test incorrect username
+        self.incorrect_login("", "password")
+        # test incorrect password
+        self.incorrect_login("admin", "")
+        
+    def incorrect_login(self, username, password):
+        """ (object, string, string) -> NoneType
+        
+        Helper function to try logging in with an incorrect parameter 
+        (either username or password).
+        Ensure the appropriate error message is received and the landing
+        page is unchanged (still on the login page).
+        """
+        driver = self.driver
+        # login
+        bell.login_test(driver, username, password)
+        sleep(5)
+        
+        # ensure appropriate error message
+        actual = Alert(driver).text
+        expected = "Login or password incorrect."
         self.assertEqual(actual, expected)
+        Alert(driver).accept() 
+        
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "login")))
+        
+        # ensure we're still on the login page
+        actual = driver.current_url
+        expected = "http://127.0.0.1:5981/apps/_design/bell/MyApp/index.html#login"
+        self.assertEqual(actual, expected)   
         
     def logout_test(self):
         """ NoneType -> NoneType
         
-        Helper function testing a correct logout operation
+        Helper function testing a correct logout operation.
         """
         driver = self.driver
         # test logout
@@ -96,13 +95,15 @@ class LoginTest(BaseCase):
     def configuration_test(self):
         """ NoneType -> NoneType
         
-        Helper function to fill in the configuration form,
-        and ensure it is successfully added to a new nation
+        Helper function filling out the configuration form and ensuring it
+        is successfully added to a new nation.
         
         TODO: Check configuration values in CouchDB         
         """
         driver = self.driver
-        fields = ["name", "code", "region", "nationName", "nationUrl", "notes"] 
+        
+        fields = ["name", "code", "region", "nationName", "nationUrl", "notes"]
+        # fill out all fields
         for field in fields:
             elem = driver.find_element_by_name(field)
             elem.send_keys("ole")
@@ -110,7 +111,8 @@ class LoginTest(BaseCase):
         # uncomment to test languages other than English
         # dropdown = Select(driver.find_element_by_name("selectLanguage"))
         # dropdown.select_by_value("Spanish")
-
+        
+        # submit the form
         submit = driver.find_element_by_id("formButton")
         submit.click()
         sleep(5)
