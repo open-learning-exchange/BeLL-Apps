1db06eccfd2ed1ca6bb0d09b36b37655597fce3d
Laxman Maharjan <email@mlaxman.com.np>
2017-01-17 08:10:50 +0545

    [#604] update the nation overview to show old community (#606)

--- app/nation/app/views/CommunityRequestsTable.js
+++ app/nation/app/views/CommunityRequestsTable.js
@@ -12,7 +12,8 @@ $(function() {
         addPendingOne: function(model) {
             if (model._id !== '_design/bell') { // only render if its NOT a design doc
                 var CommunityRow = new App.Views.CommunityRequestRow({
-                    model: model
+                    model: model,
+		    startDate: this.options.startDate
                 })
                 CommunityRow.render()
                 this.$el.append(CommunityRow.el)
@@ -22,7 +23,8 @@ $(function() {
         addOne: function(model) {
             if (model.get('_id') !== '_design/bell') { // only render if its NOT a design doc
                 var CommunityRow = new App.Views.CommunityRequestRow({
-                    model: model
+                    model: model,
+		    startDate: this.options.startDate
                 })
                 CommunityRow.render()
                 this.$el.append(CommunityRow.el)
