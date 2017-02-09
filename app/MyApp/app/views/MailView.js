2d2f090eff67dc8762d53c0c4b9228136e20b954
Raju Chaudhary <razu_latakta@hotmail.com>
2017-02-09 14:12:54 +0545

    [#609] language_attribut channged back to its original

--- app/MyApp/app/views/MailView.js
+++ app/MyApp/app/views/MailView.js
@@ -108,7 +108,7 @@ $(function() {
                 var currentdate = new Date();
                 
                     
-                    var mailBody = App.languageDict.attributes.Hello + ',<br>' + ' <b>' + username + '</b> ' + ',<br>' + App.languageDict.attributes.Your_Request_Has_Been_Accepted;
+                    var mailBody = App.languageDict.attributes.Hi + ',<br>' + ' <b>' + username + '</b> ' + ',<br>' + App.languageDict.attributes.Your_Request_Has_Been_Accepted;
                     temp = new App.Models.Mail()
                     temp.set("senderId", $.cookie('Member._id'))
                     temp.set("receiverId", mmodel.get('_id'));
@@ -138,7 +138,7 @@ $(function() {
                 var temp
                 var that = this
                 var currentdate = new Date();
-                var mailBody = App.languageDict.attributes.Hello + ',<br>' + ' <b>' + username + '</b> ' + ',<br>' + App.languageDict.attributes.Your_Request_Has_Been_Rejected;
+                var mailBody = App.languageDict.attributes.Hi + ',<br>' + ' <b>' + username + '</b> ' + ',<br>' + App.languageDict.attributes.Your_Request_Has_Been_Rejected;
                 temp = new App.Models.Mail()
                 temp.set("senderId", $.cookie('Member._id'))
                 temp.set("receiverId",  mmodel.get('_id'));
