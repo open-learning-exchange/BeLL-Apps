007b33a092d190aa2593ef1de47b2800f9dcffd7
Laxman Maharjan <email@mlaxman.com.np>
2017-02-27 18:27:19 +0545

    [#630] fixes on some language and alignment

--- app/nation/app/views/listCommunityView.js
+++ app/nation/app/views/listCommunityView.js
@@ -16,7 +16,8 @@ $(function() {
             }, 1000);
             $('#addQuestion').css('pointer-events','auto');
         },
-        render: function() {
+        render: function () {
+
             var loginOfMem = $.cookie('Member.login');
             var lang = App.Router.getLanguage(loginOfMem);
             App.languageDictValue=App.Router.loadLanguageDocs(lang);
