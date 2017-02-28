ac4c9426526e577174c5c789ae4f63030dc2b5d5
Raju Chaudhary <razu_latakta@hotmail.com>
2017-02-28 18:06:24 +0545

    [#630]-button added to viplink clicable url

--- app/nation/app/views/VipLinksView.js
+++ app/nation/app/views/VipLinksView.js
@@ -4,6 +4,14 @@ $(function() {
 
         template: $('#template-viplink').html(),
         vars:{},
+
+        events: {
+            "click button.copy": function() {
+                alert('hello');
+                // document.getElementById("addCommunity").submit();
+            }
+        },
+
         initialize: function() {
         },
 
@@ -21,3 +29,30 @@ $(function() {
     })
 
 })
+// (function() {
+
+//     'use strict';
+//   document.body.addEventListener('click', copy, true);
+//     function copy(e) {
+//     var 
+//       t = e.target,
+//       c = t.dataset.copytarget,
+//       inp = (c ? document.querySelector(c) : null);
+//     if (inp && inp.select) {
+//       inp.select();
+
+//       try {
+//         document.execCommand('copy');
+//         inp.blur();
+//         t.classList.add('copied');
+//         setTimeout(function() { t.classList.remove('copied'); }, 1500);
+//       }
+//       catch (err) {
+//         alert('please press Ctrl/Cmd+C to copy');
+//       }
+      
+//     }
+    
+//     }
+
+// })();
