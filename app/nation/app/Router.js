081547ecc6c7a17435f5780a79fa0039f7e4312e
Raju Chaudhary <razu_latakta@hotmail.com>
2017-03-20 15:57:21 +0545

    [#636] Login and language switch for viplink (#663)

--- app/nation/app/Router.js
+++ app/nation/app/Router.js
@@ -3718,6 +3718,10 @@ $(function() {
             {
                 $('#nav').hide();
                 lang = "English";
+                if($.cookie('languageFromCookie'))
+                {
+                    lang = $.cookie('languageFromCookie')
+                }
             }
             var viplinkModel = null;
             if(secretId)
@@ -3791,7 +3795,24 @@ $(function() {
                             });
                             nationConfig = nationConfig.first();
                             listCommunity = "<img src='img/logo.png' width='108px' height='108px' style='z-index:1; border:2px solid white;border-radius:60px;'/>";
-                            listCommunity = listCommunity + "<h3> " + "Hi " + viplinkModel.attributes.name + "</h3>" + "<h3>" + "Welcome to " + nationConfig.get("name") + " Communities" + "</h3>";
+                            listCommunity = listCommunity + "<h3> " + "Hi " + viplinkModel.attributes.name + "</h3>" + "<h3>" + App.languageDictValue.attributes.welcome_to + "&nbsp;" + nationConfig.get("name") + "&nbsp" + App.languageDictValue.attributes.Community + "</h3>";
+                        }
+                        if (!$.cookie('Member.login')){
+                            var options = [];
+                            var allLanguages={};
+                            var languages = new App.Collections.Languages();
+                                languages.fetch({
+                                async: false
+                            });
+                        for(var i=0;i<languages.length;i++) {
+                            if (languages.models[i].attributes.hasOwnProperty("nameOfLanguage")) {
+                                var languageName =languages.models[i].attributes.nameOfLanguage;
+                                allLanguages[languageName]=languages.models[i].get('nameInNativeLang');
+                                options += '<option value="'+languageName+'">'+ allLanguages[languageName] +'</option>';
+                            }
+                        }
+                            listCommunity += '<div align="center">'+'<select id="onLoginLanguage">'+options+'</select>'+'</div>'+'<br>';
+                            listCommunity += "<div align='center'><a class='btn btn-success' href='../MyApp/index.html#login'>" + App.languageDictValue.attributes.Sign_In + "</a></div>";
                         }
                         else {//if logged in and token is not correct
                             listCommunity = "<h3> " + App.languageDictValue.get("Communities") + "  |  <a  class='btn btn-success' id='addComm' href='#addCommunity'>" + App.languageDictValue.get("Add_Community") + "</a>  </h3><p>" + App.languageDictValue.get("Member_Resources_Count") + "</p>";
@@ -3800,11 +3821,16 @@ $(function() {
                         if (firstDt.getFullYear() != today.getFullYear() || firstDt.getMonth() != today.getMonth()) {
                             listCommunity += '<input class="date-picker"/><style>.ui-datepicker-calendar{display: none;}.date-picker{width:300px;float:right;}</style>';
                         }
-
                         listCommunity += "<div id='list-of-Communities'></div>"
 
                         App.$el.children('.body').html('<div id="communityDiv"></div>');
                         $('#communityDiv').append(listCommunity);
+                        $('#onLoginLanguage').change(function()
+                        {
+                            $.cookie('languageFromCookie',$(this).val());
+                            location.reload();
+                        });
+                        $("select option[value='"+lang+"']").attr("selected","selected");
 
                         if(firstDt.getFullYear() != today.getFullYear() || firstDt.getMonth() != today.getMonth()) {
                             $('input.date-picker').datepicker({
