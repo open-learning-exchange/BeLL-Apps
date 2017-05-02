fd6cfb4626ba84b5535adaf681250ff255539acb
paulbert <paul.r.albert@gmail.com>
2017-05-02 15:15:36 -0400

    Revert back to original openUrl & openWhichFile behavior

--- app/bell-resource-router/app/Router.js
+++ app/bell-resource-router/app/Router.js
@@ -248,9 +248,9 @@ $(function() {
             resource.on('sync', function() {
                 // If there is a openURL, that overrides what we use to open, else we build the URL according to openWith
                 if (resource.get('openUrl') && resource.get('openUrl').length > 0) {
-                    openUrl = resource.__proto__.openWithMap[resource.get('openWith')] + '/resources/' + resource.id + '/' + resource.get('openUrl')
+                    openUrl = resource.get('openUrl');
                 } else if (resource.get('openWhichFile') && resource.get('openWhichFile').length > 0) {
-                    openUrl = resource.__proto__.openWithMap[resource.get('openWith')] + '/resources/' + resource.id + '/' + resource.get('openWhichFile')
+                    openUrl = resource.__proto__.openWithMap[resource.get('openWith')] + '/resources/' + resource.id + '/' + resource.get('openWhichFile');
                 } else {
                     //We want to open only one attachment which is the latest one, so no need for secondAttachment logic
                     /*if (_.keys(resource.get('_attachments'))[1] && resource.get('openWith') != 'Bell-Reader') {
