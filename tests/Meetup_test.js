5fb50c28c23aba17a94fbf910b0cb19bbf2d5c58
pr0chin <smaharjan@himalayantechies.com>
2017-05-16 12:49:15 +0545

    [#800]: Course and Meetup Test

---
+++ tests/Meetup_test.js
@@ -0,0 +1,46 @@
+
+Feature('Courses');
+var FirstLogin = false;
+var loginCookies = [];
+
+
+Before((I) => {
+    I.login('admin', 'password');
+    I.wait(10);
+});
+
+Scenario('test successful login', (I) => {
+	var Meetup = '//*[@href="#meetups"]';
+    var Addmeetup = '//*[@href="#meetup/add"]';
+    var InviteMembers = '//*[@id="InviteMembers"]';
+    var check = '//*[@value="Weekly"]';
+    var Category = '//*[@name="category"]';
+    I.seeInCurrentUrl('#dashboard');
+    I.waitForVisible(Meetup);
+    I.waitForEnabled(Meetup);
+    I.click(Meetup);
+    I.wait(10);
+    I.waitForVisible(Addmeetup);
+    I.waitForEnabled(Addmeetup);
+    I.click(Addmeetup);
+    I.wait(10);
+    I.fillField('title', "Virtual Interns");
+    I.fillField('description', "For Upgrading Bell-App");
+    I.fillField('startDate', "05/23/2017");
+    I.fillField('endDate', "05/23/2017");
+    I.checkOption(check);
+    I.fillField('startTime', "8:00am");
+    I.fillField('endTime', "10:00am");
+    I.selectOption(Category, "ICT");
+    I.wait(5);
+    I.fillField('meetupLocation', "Kathmandu");
+    I.click(InviteMembers);
+    I.wait(20);
+
+});
+
+Scenario('test something', (I) => {
+  I.amOnPage('/');
+  I.see('Welcome');
+});
+
