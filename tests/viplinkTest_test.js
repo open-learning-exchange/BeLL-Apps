62e139124d0431270b4155f8c6285c48bb17b4ef
Raju Chaudhary <razu_latakta@hotmail.com>
2017-05-16 17:25:32 +0545

    [#809]-Delete viplink test  created

--- tests/viplinkTest_test.js
+++ tests/viplinkTest_test.js
@@ -25,3 +25,23 @@ Scenario('test CreateVipLink', (I) => {
 	I.seeInCurrentUrl('#viplink')
 	I.wait(10)
 });
+
+Scenario('test second DeleteVipLink', (I) => {
+	var managerLink = '//*[@id="NationManagerLink"]';
+	var vpPage = '//*[@href="#viplink"]';
+	var del ='//*[@class="parentDiv"]/table/tbody/tr[2]/td[5]/button[2]';
+	I.seeInCurrentUrl('#dashboard');
+	I.waitForVisible(managerLink);
+	I.waitForEnabled(managerLink);
+	I.click(managerLink);
+	I.wait(5);
+	I.click(vpPage);
+	I.seeInCurrentUrl('#viplink');
+	I.wait(5);
+	I.click(del);
+	I.seeInPopup("Are you sure that you want to delete this link?")
+	I.wait(3);
+	I.acceptPopup();
+	I.seeInCurrentUrl('#viplink');
+	I.wait(5);
+});
