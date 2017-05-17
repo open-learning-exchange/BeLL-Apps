2282292dc92f9c1d9f5a79e0be1f63bc7fd3c9a9
pr0chin <smaharjan@himalayantechies.com>
2017-05-17 11:44:18 +0545

    [#800]: Certain Change

--- tests/Meetup_test.js
+++ tests/Meetup_test.js
@@ -1,8 +1,6 @@
-
-Feature('Courses');
+Feature('Meetup');
 var FirstLogin = false;
 var loginCookies = [];
-
 
 Before((I) => {
     I.login('admin', 'password');
@@ -15,31 +13,31 @@ Scenario('Add Meetup', (I) => {
     var InviteMembers = '//*[@id="InviteMembers"]';
     var check = '//*[@value="Weekly"]';
     var Category = '//*[@name="category"]';
+    var EmailClick = '//*[@id="#mailsDash"]';
+	var Accept = '//*[@id="#invite-accept"]'
+
     I.seeInCurrentUrl('#dashboard');
     I.waitForVisible(Meetup);
     I.waitForEnabled(Meetup);
     I.click(Meetup);
-    I.wait(10);
+    I.seeInCurrentUrl('#meetups');
     I.waitForVisible(Addmeetup);
     I.waitForEnabled(Addmeetup);
     I.click(Addmeetup);
-    I.wait(10);
+    I.seeInCurrentUrl('#meetup/add');
+    I.wait(5);
     I.fillField('title', "Virtual Interns II");
-    I.fillField('description', "For switching in Angular");
+    I.fillField('description', "For switching in Angular4 and upgrading.");
     I.fillField('startDate', "05/23/2017");
     I.fillField('endDate', "05/23/2017");
     I.checkOption(check);
     I.fillField('startTime', "8:00am");
     I.fillField('endTime', "10:00am");
     I.selectOption(Category, "ICT");
-    I.wait(5);
     I.fillField('meetupLocation', "Kathmandu");
     I.click(InviteMembers);
     I.wait(5);
     I.selectOption('invitationType',"All");
-    I.wait(5);
     I.click("Invite");
-    I.wait(10);
+    I.wait(5);
 });
-
-
