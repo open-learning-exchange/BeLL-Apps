0db709ebdb3698ee38c8167610b25344662d3464
Rupesh Manandhar <rupandroo@gmail.com>
2017-06-05 14:01:37 +0545

    [#840] select value and save career

--- app/MyApp/app/views/AddCourseCareer.js
+++ app/MyApp/app/views/AddCourseCareer.js
@@ -17,38 +17,43 @@ $(function() {
         },
 
         saveCareerPath: function(){
-            var levelname = $('#level').val()
-            var coursetitle = $('#LCourse').val()
-            var arrcourseId = []
-            for(var i = 0; i <this.collection.models.length; i++){
-                if(coursetitle === this.collection.models[i].attributes.CourseTitle){
-                    var courseid = this.collection.models[i].attributes._id
-                    arrcourseId.push(courseid)
+            var selectedCourseId = []
+            var selectedCourseName = []
+            $('#LCourse option:selected').each(function(){ 
+                if ($(this).length) {
+                    selectedCourseId.push($(this).val());
+                    selectedCourseName.push($(this).text());
                 }
-            }
+            });
+            console.log(selectedCourseId, selectedCourseName)
+            var levelname = $('#levelSelect').val()
+            var courseCareerTitle = $('#careerPath').val()
             var savecoursecareer = new App.Models.CoursecareerPath()
-                savecoursecareer.set('Level_Name',levelname);
-                savecoursecareer.set('Courses',coursetitle);
-                savecoursecareer.set('CourseIds',arrcourseId);
-                savecoursecareer.set('MemberID',$.cookie('Member._id'));
-                savecoursecareer.save(null, {
-                    error: function() {
-                        console.log("Not Saved")
-                    }
-                });
-                location.reload();
+            savecoursecareer.set('Level_Name',levelname);
+            savecoursecareer.set('CoursePathName',courseCareerTitle);
+            savecoursecareer.set('Courses',selectedCourseName);
+            savecoursecareer.set('CourseIds',selectedCourseId);
+            savecoursecareer.set('MemberID',$.cookie('Member._id'));
+            savecoursecareer.save(null, {
+                error: function() {
+                    console.log("Not Saved")
+                }
+            });
+                //location.reload();
         },
 
         render: function() {
-            console.log(this.collection.models)
             var arrcourses = []
+            var arrCourseIds = []
             for(var i = 0; i <this.collection.models.length; i++){
                 var courseslist = this.collection.models[i].attributes.CourseTitle
-                console.log(courseslist)
+                var courseId = this.collection.models[i].attributes._id
                 arrcourses.push(courseslist)
+                arrCourseIds.push(courseId)
             }
             this.$el.find('.multipleCourse').append('<div class="btn btn-info" id="finishPressed">'+App.languageDict.attributes.Finish+'</div>&nbsp&nbsp');
             this.vars.Courselist = arrcourses
+            this.vars.Courseid = arrCourseIds
             this.vars.Course_Length = this.collection.models.length
             this.$el.html(_.template(this.template,this.vars))
         },
