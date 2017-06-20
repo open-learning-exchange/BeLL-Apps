8b639c9f311a8bcf5967bd4da6dabe12d7379899
pr0chin <smaharjan@himalayantechies.com>
2017-06-20 11:29:02 +0545

    [#650] Course statistic (#889)

--- app/MyApp/app/views/CoursesStatistics.js
+++ app/MyApp/app/views/CoursesStatistics.js
@@ -9,14 +9,16 @@ $(function() {
         initialize: function() {
             
         },
+
         render: function() {
             var memberName=[]
             var memberStep= []
             var membersstatus=[]
             var stepName=[]
             var stepid = []
-            var failarr = []
+            var totalerrors = []
             var totalFailStatus = []
+
                for(var i = 0; i <this.model.attributes.members.length; i++){
                     var statisticscourseProgress = new App.Collections.membercourseprogresses()
                     statisticscourseProgress.memberId = this.model.attributes.members[i];
@@ -24,11 +26,13 @@ $(function() {
                     statisticscourseProgress.fetch({
                         async:false
                     });
-                    var staticProgress = statisticscourseProgress.first()
-                    memberStep = staticProgress.attributes.stepsIds
-                    membersstatus = staticProgress.attributes.stepsStatus
-                    var pqattempts = staticProgress.attributes.pqAttempts
-                    var member = staticProgress.attributes.memberId
+                    memberStep = statisticscourseProgress.models[0].attributes.stepsIds
+                    membersstatus = statisticscourseProgress.models[0].attributes.stepsStatus
+                    var pqattempts = statisticscourseProgress.models[0].attributes.pqAttempts
+                    var member = statisticscourseProgress.models[0].attributes.memberId
+                    var failarr = []
+                    var arr = 0
+                    var arrtotalerrors = []
                     for (var k = 0; k < memberStep.length; k++)
                     {
                         var count = 0
@@ -36,10 +40,12 @@ $(function() {
                            if (membersstatus[k][j] == "0")
                             {
                                 count++
-                            } 
+                            }
                         }
-                       failarr.push(count)
+                        arr =count + arr
+                        failarr.push(count)
                     }
+                    totalerrors.push(arr)
                     totalFailStatus.push(failarr)
                     var members = new App.Models.Member({
                         _id: member
@@ -47,7 +53,7 @@ $(function() {
                     members.fetch({
                         async: false
                     });
-                   memberName.push(members.toJSON().firstName + ' ' + members.toJSON().lastName)
+                    memberName.push(members.toJSON().firstName + ' ' + members.toJSON().lastName)
                 }
                 for (var y = 0; y < memberStep.length; y++) {
                         var courseSteps = new App.Models.CourseStep()
@@ -57,17 +63,24 @@ $(function() {
                         })
                         stepid.push(courseSteps.attributes._id)
                         stepName.push(courseSteps.attributes.title)
-                    };
-                 this.vars.stepId =  stepid;
-                 this.vars.stepName =  stepName;
-                 this.vars.member_Name =  memberName;
-                 this.vars.member_sstatus = failarr;
-                 this.vars.steps = memberStep;
-                 this.vars.Totalfailstat = totalFailStatus;
-                 this.$el.html(_.template(this.template,this.vars))
+                };
+                var totalMemberstepError = []
+                for (var q = 0; q < memberStep.length; q++) {
+                     var total = 0
+                    for (var p = 0; p< this.model.attributes.members.length; p++) {
+                        total = total + totalFailStatus[p][q]
+                    }
+                    totalMemberstepError.push(total)
+                }
+                this.vars.stepId =  stepid;
+                this.vars.stepName =  stepName;
+                this.vars.member_Name =  memberName;
+                this.vars.member_sstatus = failarr;
+                this.vars.steps = memberStep;
+                this.vars.Totalfailstat = totalFailStatus;
+                this.vars.Totalerrors = totalerrors;
+                this.vars.TotalmemberSteperror = totalMemberstepError
+                this.$el.html(_.template(this.template,this.vars))
         }
-
-
     })
-
 })
