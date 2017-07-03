2fab6073e42b694045814a03c8dc87cc947e98d5
pr0chin <smaharjan@himalayantechies.com>
2017-07-03 14:50:41 +0545

    [#922] Margin-Membersarvey

--- app/MyApp/app/Router.js
+++ app/MyApp/app/Router.js
@@ -260,7 +260,7 @@ $(function() {
         SurveysForMembers: function() {
             var SurveysView = new App.Views.SurveyTableForMembers();
             SurveysView.render();
-            App.$el.children('.body').html('<div id="surveyTable"></div>');
+            App.$el.children('.body').html('<div id="surveyTable" style="margin-right:20px; margin-left:20px;"></div>');
             $('#surveyTable').append('<h3>' + App.languageDict.get('Surveys') + '</h3>');
             $('#surveyTable').append(SurveysView.el);
         },
@@ -571,7 +571,7 @@ $(function() {
                 // creditsTableView.render();
 
 
-                App.$el.children('.body').html('<div id="creditsTable"></div>');
+                App.$el.children('.body').html('<div id="creditsTable" style = "margin-left: 20px;margin-right: 20px;"></div>');
                 var course = new App.Models.Course({
                     _id: courseId
                 });
