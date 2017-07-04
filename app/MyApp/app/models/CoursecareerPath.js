9eaffa2015ebf77bcfb7c74d2d29a75e2f52ae1e
pr0chin <smaharjan@himalayantechies.com>
2017-07-04 15:27:44 +0545

    [#840]: CareerPath Updated

---
+++ app/MyApp/app/models/CoursecareerPath.js
@@ -0,0 +1,26 @@
+$(function() {
+
+    App.Models.CoursecareerPath = Backbone.Model.extend({
+
+        idAttribute: "_id",
+        url: function() {
+            if (_.has(this, 'id')) {
+                var url = (_.has(this.toJSON(), '_rev')) ? App.Server + '/coursecareerpath/' + this.id + '?rev=' + this.get('_rev') // For UPDATE and DELETE
+                    : App.Server + '/coursecareerpath/' + this.id // For READ
+            } else {
+                var url = App.Server + '/coursecareerpath' // for CREATE
+            }
+            return url
+        },
+        defaults: {
+            kind: 'coursecareerpath',//Saves kind of document according to corresponding db's.Mostly used in couch db views.
+        },
+        schema: {
+            Level_Name: 'Text', 
+            CareerPathName:'Text',
+            Courselist: 'Text', 
+            CourseIds:[],//Array:Multiple Courses
+            MemberID:'Text'
+        },
+    })
+})
