0012ae0b6a49382ec3b6fdda80cde071d5714fe0
Raju Chaudhary <razu_latakta@hotmail.com>
2017-07-31 11:53:36 +0545

    [#952] CourseMem Filter by community added

--- app/MyApp/app/views/CourseMembers.js
+++ app/MyApp/app/views/CourseMembers.js
@@ -2,7 +2,6 @@ $(function () {
 
     App.Views.CourseMembers = Backbone.View.extend({
         vars: {},
-
         initialize: function () {},
 
         removeMember:function(e){
@@ -12,7 +11,7 @@ $(function () {
                 _id: this.courseId
             })
             courseModel.fetch({
-         		success:function(result){
+                success:function(result){
                     var members = result.get('members')
                     members.splice(members.indexOf(memberId),1)
                     result.set('members',members)
@@ -24,11 +23,11 @@ $(function () {
                         async:false
                     })
                     while (model = memberCoursePro.first()) {
-						model.destroy();
-	                }
+                        model.destroy();
+                    }
                     that.render()
                     alert(App.languageDict.attributes.Member_Removed_From_Course)
-         		}
+                }
             })  
         },
 
@@ -47,12 +46,13 @@ $(function () {
             config.fetch({
                 async: false
             })
+
             var currentConfig = config.first().toJSON()
             var code = currentConfig.rows[0].doc.code
             var na = currentConfig.rows[0].doc.nationName.substring(3,5);
             $('.courseEditStep').empty();
             $('.courseEditStep').append('<h3>'+App.languageDict.attributes.Course_Members+ ' | ' + courseModel.get('name') + '</h3>')
-            var viewtext = '<table class="btable btable-striped"><th>'+App.languageDict.attributes.Photo+'</th><th>'+App.languageDict.attributes.Name+'</th><th>'+App.languageDict.attributes.Roles+'</th><th colspan=2>'+App.languageDict.attributes.Actions+'</th>'
+            var viewtext = '<table id = "Table1" class="btable btable-striped"><th>'+App.languageDict.attributes.Photo+'</th><th>'+App.languageDict.attributes.Name+'</th><th>'+App.languageDict.attributes.Roles+'</th><th colspan=2>'+App.languageDict.attributes.Actions+'</th>'
             for (var i = 0; i < memberList.length; i++) {
                 var mem = new App.Models.Member({
                     _id: memberList[i]
@@ -94,9 +94,93 @@ $(function () {
                 'onclick=showComposePopupMultiple("' + mail + '") id="sendMailButton">'
                 +App.languageDict.attributes.Send_Email+'</button>' +
                 '<button class="btn"   id="retrunBack" onclick=retrunBack()>'
-                +App.languageDict.attributes.Back+'</button></td></tr>';
+                +App.languageDict.attributes.Back+'</button></td><td></td><td></td></tr>';
             viewtext += '</table>';
             $('.courseEditStep').append(viewtext)
+        },
+        randerTable: function(selectedvalue){
+            $('#Table1').remove()
+             var courseModel = new App.Models.Course({
+                _id: this.courseId
+            })
+            courseModel.fetch({
+                async: false
+            })
+            var memberList = courseModel.get('members')
+            var configurations = Backbone.Collection.extend({
+                url: App.Server + '/configurations/_all_docs?include_docs=true'
+            })
+            var config = new configurations()
+            config.fetch({
+                async: false
+            })
+            var currentConfig = config.first().toJSON()
+            var code = currentConfig.rows[0].doc.code
+            var na = currentConfig.rows[0].doc.nationName.substring(3,5);
+            $('.courseEditStep').empty();
+            $('.courseEditStep').append('<h3>'+App.languageDict.attributes.Course_Members+ ' | ' + courseModel.get('name') + '</h3>')
+            var viewtext = '<table id = "Table1" class="btable btable-striped"><th>'+App.languageDict.attributes.Photo+'</th><th>'+App.languageDict.attributes.Name+'</th><th>'+App.languageDict.attributes.Roles+'</th><th colspan=2>'+App.languageDict.attributes.Actions+'</th>'
+            var newMemberList = [];
+            for (var i = 0; i < memberList.length; i++) {
+                var mem = new App.Models.Member({
+                    _id: memberList[i]
+                })
+                mem.fetch({
+                    async: false
+                })
+                if(mem.attributes.community == selectedvalue){
+                    newMemberList.push(memberList[i]);
+                }
+            }
+            if(newMemberList.length > 0){
+                for (var i = 0; i < newMemberList.length; i++) {
+                    var mem = new App.Models.Member({
+                        _id: newMemberList[i]
+                    })
+                    mem.fetch({
+                        async: false
+                    })
+                    var roleOfMem;
+                    if(courseModel.get('courseLeader').indexOf(mem.get('_id')) > -1) {
+                        roleOfMem=App.languageDict.attributes.Leader
+                    } else {
+                        roleOfMem=App.languageDict.attributes.Learner
+                    }
+                    var mail = mem.get('login') + '.' + code +na+ '@olebell.org'
+                    var src = "img/default.jpg"
+                    var attchmentURL = '/members/' + mem.id + '/'
+                    if (typeof mem.get('_attachments') !== 'undefined') {
+                        attchmentURL = attchmentURL + _.keys(mem.get('_attachments'))[0]
+                        src = attchmentURL
+                    }
+                    viewtext += '<tr><td><img width="45px" height="45px" src="' + src + '"/></td><td>' + mem.get('firstName') + ' ' + mem.get('lastName') + '</td><td>'+roleOfMem+'</td><td><input type="checkbox" name="courseMember" value="' + mail + '">'+App.languageDict.attributes.Send_Email+'</td>'
+                    var loggedIn = new App.Models.Member({
+                        "_id": $.cookie('Member._id')
+                    })
+                    loggedIn.fetch({
+                        async: false
+                    })
+                    var roles = loggedIn.get("roles")
+                    if( courseModel.get('courseLeader') && courseModel.get('courseLeader').indexOf($.cookie('Member._id'))>-1 || roles.indexOf('Manager')>-1) {
+                        var memId=mem.get('_id')+','+this.courseId;
+                        viewtext+='<td><button class="btn btn-danger removeMember" value="' + mem.get('_id') + '" onclick=removeMemberFromCourse(\"' +  memId + '")>'+App.languageDict.attributes.Remove+'</button></td>'
+                    }
+                    viewtext+='</tr>'
+                }
+                viewtext += '<tr><td></td><td></td><td>' +
+                '<button class="btn"  id="selectAllMembersOnMembers" onclick=selectAllMembers()>' +
+                App.languageDict.attributes.Select_All+'</button>' +
+                '<button style="" class="btn" ' +
+                'onclick=showComposePopupMultiple("' + mail + '") id="sendMailButton">'
+                +App.languageDict.attributes.Send_Email+'</button>' +
+                '<button class="btn"   id="retrunBack" onclick=retrunBack()>'
+                +App.languageDict.attributes.Back+'</button></td><td></td><td></td></tr>';
+            viewtext += '</table>';
+            $('.courseEditStep').append(viewtext)
+            } else {
+                viewtext += '<tr><td></td><td></td><td></td><td></td></tr>';
+                $('.courseEditStep').append(viewtext)
+            }
         }
     })
 })
