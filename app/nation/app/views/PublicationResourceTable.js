16fcb755d640b3db915c8014a87104361224a2d8
Rupesh Manandhar <rupandroo@gmail.com>
2017-08-30 17:20:57 +0545

    [#1006] Nation publication design fix (#1011)

--- app/nation/app/views/PublicationResourceTable.js
+++ app/nation/app/views/PublicationResourceTable.js
@@ -21,7 +21,7 @@ $(function() {
         },
 
         addAll: function() {
-            this.$el.append('<tr><th>'+App.languageDictValue.get('Title')+'</th><th colspan="2">'+App.languageDictValue.get('Actions')+'</th></tr>')
+            this.$el.append('<tr><th>'+App.languageDictValue.get('Title')+'</th><th colspan="2" width="30%">'+App.languageDictValue.get('Actions')+'</th></tr>')
             if (this.collection.length == 0)
                 this.$el.append('<tr><td colspan="2">'+App.languageDictValue.get('No_Resource_Pub')+'<td></tr>')
             this.collection.forEach(this.addOne, this)
